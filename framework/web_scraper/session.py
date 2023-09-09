a9a5ec943312ecdd08d3613b100540f0149ef25d
Ben <ben.talese@gmail.com>
2023-09-09 10:12:20 +1000

    Fix woolies session bug
    User-agent was either too long or woolies API didn't like something in it so it would take forever to process the request. Also added caching.

--- framework/web_scraper/session.py
+++ framework/web_scraper/session.py
@@ -1,6 +1,7 @@
 import requests
 from requests import PreparedRequest, Response
 from requests.adapters import HTTPAdapter, Retry
+import requests_cache
 
 
 class DefaultTimeoutAdapter(HTTPAdapter):
@@ -14,24 +15,22 @@ class DefaultTimeoutAdapter(HTTPAdapter):
 
 
 def create_session(
-        user_agent: str = "DiscountDora/1.0.0 (ben.talese@gmail.com)",
+        user_agent: str = "DiscountDora",
         max_retries: int = 3,
-        timeout: int = 10) -> requests.Session:
-    session = requests.Session()
-
-    session.headers.update({'User-Agent': user_agent})
+        timeout: int = 10) -> requests_cache.CachedSession:
+    session = requests_cache.CachedSession(backend='memory')
 
     retry_strategy = Retry(
         total=max_retries,
         backoff_factor = 2,
         status_forcelist=[429, 500, 502, 503, 504]
     )
+    session.mount('http://', DefaultTimeoutAdapter(timeout=timeout, max_retries=retry_strategy))
+    session.mount('https://', DefaultTimeoutAdapter(timeout=timeout, max_retries=retry_strategy))
 
     session.hooks = {
         'response': lambda r, *args, **kwargs: r.raise_for_status()
     }
-
-    session.mount('http://', DefaultTimeoutAdapter(timeout=timeout, max_retries=retry_strategy))
-    session.mount('https://', DefaultTimeoutAdapter(timeout=timeout, max_retries=retry_strategy))
+    session.headers.update({'User-Agent': user_agent})
 
     return session

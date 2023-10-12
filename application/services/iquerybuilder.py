215cd059b0ec7e35dc1724c1ef6b0acb6b8f4b60
Ben <ben.talese@gmail.com>
2023-10-12 23:13:00 +1100

    Match querybuilder interface to implementation

--- application/services/iquerybuilder.py
+++ application/services/iquerybuilder.py
@@ -1,32 +1,48 @@
-# TODO: Is this the right spot for this file?
-
 from abc import ABC
+from typing import Callable
+
+from application.infrastructure.bool_operation import BoolOperation
 
 
 class IQueryBuilder(ABC):
-    def any(self, condition = None):
+    def any(self, condition: BoolOperation = None):
         return self
 
     def execute(self):
         pass
 
-    def first(self, condition = None):
+    def first(self, condition: BoolOperation = None):
         return self
 
     def first_by_id(self, *ids):
+        '''
+        `HINT/USAGE`
+        Single id: first_by_id(1)
+        Composite id style 1: first_by_id(1, 2) `Order must match order of column definitions`
+        Composite id style 2: first_by_id({"id1": 1, "id2": 2})
+        '''
         return self
 
-    def first_or_none(self, condition = None):
+    def first_by_id_or_none(self, *ids):
+        '''
+        `HINT/USAGE`
+        Single id: first_by_id(1)
+        Composite id style 1: first_by_id(1, 2) `Order must match order of column definitions`
+        Composite id style 2: first_by_id({"id1": 1, "id2": 2})
+        '''
         return self
 
-    def include(self, attribute_name):
+    def first_or_none(self, condition: BoolOperation = None):
+        return self
+
+    def include(self, attribute_name: str):
        return self
 
-    def project(self, *attributes):
+    def project(self, func: Callable):
         return self
 
-    def then_include(self, attribute_name):
+    def then_include(self, attribute_name: str):
         return self
 
-    def where(self, condition):
+    def where(self, condition: BoolOperation):
         return self

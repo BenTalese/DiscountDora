4a8d1a2134ce96c4a89a6142e2c6e1a8806969e3
Ben <ben.talese@gmail.com>
2023-10-24 21:17:23 +1100

    Refactor default pipeline placement

--- interface_adaptors/controllers/base_controller.py
+++ interface_adaptors/controllers/base_controller.py
@@ -1,5 +1,8 @@
 from clapy import IUseCaseInvoker
 
+from application.pipeline.pipeline_configuration import PipelineConfiguration
+
+DEFAULT_PIPELINE = PipelineConfiguration.DefaultConfiguration.value
 
 class BaseController:
     def __init__(self, use_case_invoker: IUseCaseInvoker):
--- interface_adaptors/controllers/stock_item_controller.py
+++ interface_adaptors/controllers/stock_item_controller.py
@@ -2,21 +2,18 @@ from typing import List
 
 from clapy import PipeConfiguration
 
-from application.pipeline.pipeline_configuration import PipelineConfiguration
 from application.use_cases.stock_items.get_stock_items.get_stock_items_input_port import \
     GetStockItemsInputPort
 from application.use_cases.stock_items.get_stock_items.iget_stock_items_output_port import \
     IGetStockItemsOutputPort
 
-from .base_controller import BaseController
+from .base_controller import DEFAULT_PIPELINE, BaseController
 
 
 class StockItemController(BaseController):
 
-    default_config = PipelineConfiguration.DefaultConfiguration.value
-
     async def get_stock_items_async(
             self,
             output_port: IGetStockItemsOutputPort,
-            pipeline_configuration: List[PipeConfiguration] = default_config):
+            pipeline_configuration: List[PipeConfiguration] = DEFAULT_PIPELINE):
         await self._use_case_invoker.invoke_usecase_async(GetStockItemsInputPort(), output_port, pipeline_configuration)

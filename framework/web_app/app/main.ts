dc0f869ea016ea130b14c2b3a4683a02b6f0f969
Ben <ben.talese@gmail.com>
2023-11-04 11:46:51 +1100

    Use bootstrap

--- framework/web_app/app/main.ts
+++ framework/web_app/app/main.ts
@@ -1,8 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/main.css'
 
 const app = createApp(App)
 

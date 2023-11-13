b633a1d4aea1f474988abd5904464b7d8a266aff
Ben <ben.talese@gmail.com>
2023-11-13 22:11:57 +1100

    Modify properties of domain

--- domain/entities/merchant.py
+++ domain/entities/merchant.py
@@ -6,4 +6,4 @@ from domain.entities.base_entity import BaseEntity
 @dataclass
 class Merchant(BaseEntity):
     name: str = None
-    url: str = None # hmm...does this belong in this domain?
+    # url: str = None # hmm...does this belong in this domain?
--- domain/entities/product.py
+++ domain/entities/product.py
@@ -1,26 +1,21 @@
 from dataclasses import dataclass
 from typing import List
-from domain.entities.base_entity import BaseEntity
 
+from domain.entities.base_entity import BaseEntity
 from domain.entities.merchant import Merchant
 from domain.entities.product_offer import ProductOffer
 
 
-# # TODO PERHAPS THIS SHOULD JUST BE FLATTENED ON THE PRODUCT?
-# @dataclass
-# class Measurement(BaseEntity): # TODO: This should be owned in DB
-#     quantity: int = None
-#     unit: str = None
-
 @dataclass
 class Product(BaseEntity):
     brand: str = None
-    current_offer: ProductOffer = None # flatten onto Product?
-    # historical_offers: List[ProductOffer] = None
-    image: str = None
+    current_offer: ProductOffer = None
+    historical_offers: List[ProductOffer] = None
+    image: bytes = None
     is_available: bool = None
     merchant: Merchant = None
+    merchant_stockcode: str = None
     name: str = None
     size_unit: str = None
-    size_value: str = None
+    size_value: float = None
     web_url: str = None

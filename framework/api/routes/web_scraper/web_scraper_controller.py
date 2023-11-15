72ccc662e5811ddd310faf5fcfdd06ac1dee75b3
Ben <ben.talese@gmail.com>
2023-11-15 21:04:57 +1100

    Fix naming of route

--- framework/api/routes/web_scraper/web_scraper_controller.py
+++ framework/api/routes/web_scraper/web_scraper_controller.py
@@ -4,8 +4,8 @@ from framework.web_scraper.controller import search_for_product
 
 web_scraper = Blueprint("web_scraper", __name__, url_prefix="/api/webScraper")
 
-@web_scraper.route("/doTheThing", methods=["POST"])
-async def do_the_thing():
+@web_scraper.route("/search", methods=["POST"])
+async def search_for_product_async():
     x = search_for_product(request.get_json()['searchTerm'], request.get_json()['startPage'])
     for y in x:
         y.image = y.image.decode('utf-8') # TODO: do in view model mapping

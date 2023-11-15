6ddcfca30e94dcf810a4a59f472739ac5d7ea6f0
Ben <ben.talese@gmail.com>
2023-11-15 21:05:15 +1100

    Comment out template styling

--- framework/web_app/app/main.ts
+++ framework/web_app/app/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import './assets/main.css'
+// import './assets/main.css'
 
 const app = createApp(App)
 

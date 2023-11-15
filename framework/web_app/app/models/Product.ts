f4d43a8cbc7673e3517ea0c792c17345c53b5963
Ben <ben.talese@gmail.com>
2023-11-15 20:59:51 +1100

    Create Product.ts

---
+++ framework/web_app/app/models/Product.ts
@@ -0,0 +1,13 @@
+export type Product = {
+    brand: string
+    image: string
+    is_available: boolean
+    merchant: string
+    merchant_stockcode: string
+    name: string
+    price_now: number
+    price_was: number
+    size_unit: string
+    size_value: number
+    web_url: string
+}

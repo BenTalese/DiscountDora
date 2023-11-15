5fcbf78e9a41b8eaff97dd9a71ddbd7c9adcf3ac
Ben <ben.talese@gmail.com>
2023-11-15 20:59:35 +1100

    Import new models

--- framework/persistence/models/__init__.py
+++ framework/persistence/models/__init__.py
@@ -1,4 +1,7 @@
 from .association_tables import ShoppingListStockItemAssociation
+from .merchant_model import MerchantModel
+from .product_model import ProductModel
+from .product_offer_model import ProductOfferModel
 from .shopping_list_model import ShoppingListModel
 from .stock_item_model import StockItemModel
 from .stock_level_model import StockLevelModel

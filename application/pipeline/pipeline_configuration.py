ac7ca3ae9ca22591be745b8478226c6e60d273fc
Ben <ben.talese@gmail.com>
2023-11-17 17:19:56 +1100

    Configure more pipes for pipeline

--- application/pipeline/pipeline_configuration.py
+++ application/pipeline/pipeline_configuration.py
@@ -1,12 +1,19 @@
 from enum import Enum
 
-from clapy import InputTypeValidator, Interactor, PipeConfiguration, PipeConfigurationOption, RequiredInputValidator
+from clapy import (AuthenticationVerifier, AuthorisationEnforcer,
+                   EntityExistenceChecker, InputPortValidator,
+                   InputTypeValidator, Interactor, PipeConfiguration,
+                   PipeConfigurationOption, RequiredInputValidator)
 
 
 class PipelineConfiguration(Enum):
 
     DefaultConfiguration = [
+        PipeConfiguration(AuthenticationVerifier),
+        PipeConfiguration(AuthorisationEnforcer),
         PipeConfiguration(RequiredInputValidator, PipeConfigurationOption.INSERT, True),
         PipeConfiguration(InputTypeValidator, PipeConfigurationOption.INSERT, True),
+        PipeConfiguration(EntityExistenceChecker),
+        PipeConfiguration(InputPortValidator),
         PipeConfiguration(Interactor)
     ]

92eb1fa5404eb037ad077e7f41b78f5a4039f336
Ben <ben.talese@gmail.com>
2023-11-22 19:47:01 +1100

    ABC

--- application/use_cases/products/create_product/create_product_input_port.py
+++ application/use_cases/products/create_product/create_product_input_port.py
@@ -5,13 +5,13 @@ from domain.entities.base_entity import EntityID
 
 class CreateProductInputPort(InputPort):
     brand: str = None
-    price_now: float
-    price_was: float
     image: bytes = None
     is_available: bool
     merchant_id: EntityID
     merchant_stockcode: str
     name: str
+    price_now: float
+    price_was: float
     size_unit: str
     size_value: float = 0 # TODO: Can be nothing?
     web_url: str

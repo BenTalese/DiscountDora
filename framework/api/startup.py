6eea637724c4bc3804af6a04c06f69a9a7778f5d
Ben <ben.talese@gmail.com>
2023-11-22 19:51:16 +1100

    Clean up and rename

--- framework/api/startup.py
+++ framework/api/startup.py
@@ -1,38 +1,33 @@
-import asyncio
-import json
 import os
 import sys
-from typing import List
-
-from flask_cors import CORS
 
 sys.path.append(os.getcwd())
 
+import asyncio
+import json
+
 from clapy import DependencyInjectorServiceProvider
 from flask import Flask
+from flask_cors import CORS
 
 from framework.api.error_handlers import error_handlers
 from framework.api.middleware import middleware
-from framework.api.routes.stock_items.stock_items_controller import stock_items
-from framework.api.routes.web_scraper.web_scraper_controller import web_scraper
+from framework.api.routes.products.product_router import product_router
+from framework.api.routes.stock_items.stock_item_router import \
+    stock_item_router
+from framework.api.routes.web_scraper.web_scraper_router import \
+    web_scraper_router
 from framework.api.service_collection_builder import ServiceCollectionBuilder
 from framework.persistence.infrastructure.persistence_context import \
     SqlAlchemyPersistenceContext
-from interface_adaptors.controllers.stock_item_controller import \
-    StockItemController
 
 
-#FIXME: Carefully consider the placement of this file and what it does...
-# The services here might be API specific, or maybe everything is hosted under the
-# umbrella of the web app and it makes sense...not sure...
 async def startup():
-    service_provider = ServiceCollectionBuilder(DependencyInjectorServiceProvider()).build_service_provider()
-
     app = Flask(__name__)
 
-    CORS(app, resources={r'/api/*': {'origins': 'http://localhost:5173', "allow_headers": "*"}})
+    CORS(app, resources={r'/api/*': {'origins': 'http://localhost:5173', "allow_headers": ["*", "Content-Type"]}})
 
-    app.service_provider = service_provider
+    app.service_provider = ServiceCollectionBuilder(DependencyInjectorServiceProvider()).build_service_provider()
 
     basedir = os.path.abspath(os.path.dirname(__file__))
     with open(os.path.join(basedir,'appsettings.json'), 'r') as _Configuration:
@@ -43,21 +38,21 @@ async def startup():
     app.register_blueprint(middleware)
     app.register_blueprint(error_handlers)
 
-    register_controllers(app)
+    register_routers(app)
 
-    app.stock_item_controller = service_provider.get_service(StockItemController)
     await SqlAlchemyPersistenceContext.test(app)
 
     app.run()
 
-    # app.add_url_rule("/", view_func=routes.index, methods=['GET'])
-    # app.add_url_rule("/search", view_func=routes.add_stock_item, methods=['POST'])
+def register_routers(app: Flask):
+    app.register_blueprint(stock_item_router)
+    app.register_blueprint(web_scraper_router)
+    app.register_blueprint(product_router)
 
-    # _Controller: StockItemController = service_provider.get_service(StockItemController)
-    # gg = QuickTestPresenter()
-    # await _Controller.get_stock_items_async(gg)
+if __name__ == '__main__':
+    asyncio.run(startup())
 
-    # v = 0
+
 
 # bcrypt = Bcrypt().init_app(app) # TODO: Look into this
 
@@ -68,21 +63,5 @@ async def startup():
 #     migrate.init_app(app, db)
 #     bcrypt
 
-#     return app
-
-
-    # Example of simple default use case invocation:
-
-
-    # testentity = Test(uuid.uuid4(), "test")
-    # testconfig = TestConfiguration(**testentity.__dict__)
-
     # migrate = Migrate(app, db).??? #TODO: Learn https://flask-migrate.readthedocs.io/en/latest/index.html
     #debug_mode = app.config.get('DEBUG')
-
-def register_controllers(app: Flask):
-    app.register_blueprint(stock_items)
-    app.register_blueprint(web_scraper)
-
-if __name__ == '__main__':
-    asyncio.run(startup())

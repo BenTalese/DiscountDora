ae3f9ba9bcd8c8123a560a3b88a79d8a2c8c3ef6
Ben <ben.talese@gmail.com>
2023-11-23 23:09:17 +1100

    Use UUID instead of uuid

--- framework/api/view_models/stock_item_view_model.py
+++ framework/api/view_models/stock_item_view_model.py
@@ -1,15 +1,15 @@
 from dataclasses import dataclass
 import datetime
-import uuid
+from uuid import UUID
 
 from application.dtos.stock_item_dto import StockItemDto
 
 @dataclass
 class StockItemViewModel:
     name: str
-    stock_item_id: uuid
-    stock_level_id: uuid
-    stock_location_id: uuid
+    stock_item_id: UUID
+    stock_level_id: UUID
+    stock_location_id: UUID
     stock_level_last_updated: datetime
 
 def get_stock_item_view_model(stock_item: StockItemDto) -> StockItemViewModel:

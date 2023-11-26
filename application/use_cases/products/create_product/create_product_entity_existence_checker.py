8f83067abeef97a65ae7ea0ce9c050c39ca07cf3
Ben <ben.talese@gmail.com>
2023-11-26 15:49:42 +1100

    Add missing parameter

--- application/use_cases/products/create_product/create_product_entity_existence_checker.py
+++ application/use_cases/products/create_product/create_product_entity_existence_checker.py
@@ -3,6 +3,7 @@ from application.services.ientity_existence_checker import IEntityExistenceCheck
 
 from application.use_cases.products.create_product.create_product_input_port import CreateProductInputPort
 from application.use_cases.products.create_product.icreate_product_output_port import ICreateProductOutputPort
+from domain.entities.merchant import Merchant
 
 
 class CreateProductEntityExistenceChecker(EntityExistenceChecker):
@@ -11,6 +12,6 @@ class CreateProductEntityExistenceChecker(EntityExistenceChecker):
         self.existence_checker = existence_checker
 
     async def execute_async(self, input_port: CreateProductInputPort, output_port: ICreateProductOutputPort):
-        if not self.existence_checker.does_entity_exist(input_port.merchant_id):
+        if not self.existence_checker.does_entity_exist(Merchant, input_port.merchant_id):
             self.has_failures = True
             await output_port.present_merchant_not_found_async(input_port.merchant_id)

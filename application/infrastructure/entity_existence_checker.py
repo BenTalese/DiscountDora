4f580065f00fb77cb41b2c2492de1380d7640021
Ben <ben.talese@gmail.com>
2023-11-26 15:58:37 +1100

    Fix implementation of existence checker
    Remove support for multiple IDs because fuck that noise

--- application/infrastructure/entity_existence_checker.py
+++ application/infrastructure/entity_existence_checker.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List
 from application.services.ientity_existence_checker import IEntityExistenceChecker
 from application.services.ipersistence_context import IPersistenceContext
 from domain.entities.base_entity import EntityID
@@ -10,10 +10,10 @@ class EntityExistenceChecker(IEntityExistenceChecker):
     def __init__(self, persistence_context: IPersistenceContext):
         self.persistence_context = persistence_context
 
-    def does_entity_exist(self, entity_type: TEntity, *entity_ids: Tuple[EntityID]) -> bool:
+    def does_entity_exist(self, entity_type: TEntity, entity_id: EntityID) -> bool:
         return self.persistence_context \
             .get_entities(entity_type) \
-            .first_by_id_or_none(*entity_ids) != None
+            .first_by_id_or_none(entity_id) != None
 
-    def do_entities_exist(self, entities: List[TEntity]) -> bool:
-        return all(self.does_entity_exist(entity) for entity in entities)
+    def do_entities_exist(self, entity_type: TEntity, entity_ids: List[EntityID]) -> bool:
+        return all(self.does_entity_exist(entity_type, entity_id) for entity_id in entity_ids)
--- application/services/ientity_existence_checker.py
+++ application/services/ientity_existence_checker.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List, Tuple
+from typing import List
 from domain.entities.base_entity import EntityID
 
 from domain.generics import TEntity
@@ -8,9 +8,9 @@ from domain.generics import TEntity
 class IEntityExistenceChecker(ABC):
 
     @abstractmethod
-    def does_entity_exist(self, entity_type: TEntity, *entity_ids: Tuple[EntityID]) -> bool:
+    def does_entity_exist(self, entity_type: TEntity, entity_id: EntityID) -> bool:
         pass
 
     @abstractmethod
-    def do_entities_exist(self, entity_type: TEntity, entity_ids: List[Tuple[EntityID]]) -> bool:
+    def do_entities_exist(self, entity_type: TEntity, entity_ids: List[EntityID]) -> bool:
         pass

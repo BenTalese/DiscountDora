e86a19d1cb95c4bd5ba3c9d85bbc36544cd7ab90
Ben <ben.talese@gmail.com>
2023-11-26 16:27:40 +1100

    Add todos for clapy

--- application/use_cases/products/create_product/create_product_input_port.py
+++ application/use_cases/products/create_product/create_product_input_port.py
@@ -3,7 +3,16 @@ from clapy import InputPort
 from domain.entities.base_entity import EntityID
 
 
+# TODO: clapy should have this ctor on input port class, test if plays well with required input enforcement
+# TODO: also, DependencyInjectorServiceProvider.get_service() is shit with error message (none at all) for
+# when interface mismatches implementation (method signatures), and also when it just can't construct the
+# service (e.g. dependencies registered in the wrong order)
+# TODO: Errors in previous pipes not checked?? Check clapy, i thought it would check all prev pipes or something
 class CreateProductInputPort(InputPort):
+    # TODO: Maybe just delete this? I feel explicit mapping is best
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
     brand: str = None
     image: bytes = None
     is_available: bool

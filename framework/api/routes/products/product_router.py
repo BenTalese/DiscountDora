cd7ea8fd76eeb0da34b944dee2b7fbdbd31b1eee
Ben <ben.talese@gmail.com>
2023-11-26 16:37:08 +1100

    Update create product endpoint

--- framework/api/routes/products/product_router.py
+++ framework/api/routes/products/product_router.py
@@ -1,8 +1,14 @@
 from clapy import IServiceProvider
-from flask import Blueprint, current_app, make_response, request
-from application.use_cases.products.create_product.create_product_input_port import CreateProductInputPort
-from framework.api.routes.products.create_product_presenter import CreateProductPresenter
+from flask import Blueprint, current_app, request
+from varname import nameof
 
+from application.use_cases.products.create_product.create_product_input_port import \
+    CreateProductInputPort
+from domain.entities.base_entity import EntityID
+from framework.api.routes.products.create_product_command import \
+    CreateProductCommand
+from framework.api.routes.products.create_product_presenter import \
+    CreateProductPresenter
 from interface_adaptors.controllers.product_controller import ProductController
 
 PRODUCT_ROUTER = Blueprint("product_router", __name__, url_prefix="/api/products")
@@ -12,8 +18,23 @@ async def create_product_async():
     _ServiceProvider: IServiceProvider = current_app.service_provider
     _ProductController: ProductController = _ServiceProvider.get_service(ProductController)
     _Presenter: CreateProductPresenter = _ServiceProvider.get_service(CreateProductPresenter)
-    _Presenter.get_route = get_products_async
-    await _ProductController.create_product_async(CreateProductInputPort(**request.request_object), _Presenter)
+
+    _Presenter.get_route = f"{nameof(PRODUCT_ROUTER)}.{nameof(get_products_async)}"
+    _Command: CreateProductCommand = request.request_object
+    _InputPort = CreateProductInputPort(
+        brand = _Command.brand,
+        image = _Command.image,
+        is_available = _Command.is_available,
+        merchant_id = EntityID(_Command.merchant_id),
+        merchant_stockcode = _Command.merchant_stockcode,
+        name = _Command.name,
+        price_now = _Command.price_now,
+        price_was = _Command.price_was,
+        size_unit = _Command.size_unit,
+        size_value = _Command.size_value,
+        web_url = _Command.web_url)
+
+    await _ProductController.create_product_async(_InputPort, _Presenter)
     return _Presenter.result
 
 

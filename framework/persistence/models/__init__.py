843087fcdb55f9eef13393e7a97b6da670f06734
Ben <ben.talese@gmail.com>
2023-11-27 17:20:05 +1100

    Create user model

--- framework/persistence/models/__init__.py
+++ framework/persistence/models/__init__.py
@@ -6,3 +6,4 @@ from .shopping_list_model import ShoppingListModel
 from .stock_item_model import StockItemModel
 from .stock_level_model import StockLevelModel
 from .stock_location_model import StockLocationModel
+from .user_model import UserModel
---
+++ framework/persistence/models/user_model.py
@@ -0,0 +1,34 @@
+from uuid import uuid4
+from sqlalchemy import Column, String
+from sqlalchemy_utils import UUIDType
+
+from domain.entities.base_entity import EntityID
+from domain.entities.stock_location import StockLocation
+from domain.entities.user import User
+from framework.persistence.infrastructure.persistence_context import db
+
+
+class UserModel(db.Model):
+    __entity__ = User
+    __tablename__ = User.__name__
+
+    id = Column(
+        UUIDType,
+        primary_key=True,
+        default=uuid4)
+
+    email = Column(String(255))
+
+    send_deals_on_day = Column(String(255))
+
+    username = Column(String(255))
+
+    def to_entity(self) -> User:
+        return User(
+            id = EntityID(self.id),
+            email = self.email,
+            send_deals_on_day = self.send_deals_on_day,
+            username = self.username)
+
+    def get_key(self):
+        return self.id

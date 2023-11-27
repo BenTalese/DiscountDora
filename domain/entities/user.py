23a1d0a0c58a64154009f53dfb98395520a5a04c
Ben <ben.talese@gmail.com>
2023-11-27 17:19:29 +1100

    Update user domain

--- domain/entities/user.py
+++ domain/entities/user.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 from domain.entities.base_entity import BaseEntity
 
@@ -6,4 +7,6 @@ from domain.entities.base_entity import BaseEntity
 @dataclass
 class User(BaseEntity):
     email: str = None
-    name: str = None
+    send_deals_on_day: datetime = None
+    username: str = None
+    # TODO: auth

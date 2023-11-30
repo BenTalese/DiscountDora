dccf61731e680f9f750a3a0162b00ba7ab365304
Ben <ben.talese@gmail.com>
2023-11-30 18:55:18 +1100

    Update config

--- framework/web_app/vite.config.ts
+++ framework/web_app/vite.config.ts
@@ -1,16 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./app', import.meta.url))
+    plugins: [
+        vue(),
+    ],
+    server: {
+        port: 5174,
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./app', import.meta.url))
+        }
     }
-  }
-})
+});

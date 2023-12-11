221b15cfffaa15d26fb36678f6385109e693468b
Ben <ben.talese@gmail.com>
2023-12-11 15:07:21 +1100

    Fix input port and add bug to-do

--- application/use_cases/products/update_product/update_product_input_port.py
+++ application/use_cases/products/update_product/update_product_input_port.py
@@ -1,10 +1,15 @@
 from clapy import InputPort
-from application.infrastructure.attribute_change_tracker import AttributeChangeTracker
 
+from application.infrastructure.attribute_change_tracker import \
+    AttributeChangeTracker
 from domain.entities.base_entity import EntityID
 
 
+# BUG: Clapy InputTypeValidator needs to do "type(type_hint)" to work with AttributeChangeTracker
 class UpdateProductInputPort(InputPort):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
     is_available: AttributeChangeTracker[bool] = AttributeChangeTracker[bool]()
     price_now: AttributeChangeTracker[float] = AttributeChangeTracker[float]()
     price_was: AttributeChangeTracker[float] = AttributeChangeTracker[float]()

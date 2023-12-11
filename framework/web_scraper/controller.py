b4a0cffd9e0bd3801cfbc015c85aad203394e162
Ben <ben.talese@gmail.com>
2023-12-12 08:40:27 +1100

    Use attribute change trackers

--- framework/web_scraper/controller.py
+++ framework/web_scraper/controller.py
@@ -1,25 +1,21 @@
-import base64
-import os
-import sys
+from base64 import b64encode
+from typing import List
 
 from application.dtos.product_dto import ProductDto
+from application.infrastructure.attribute_change_tracker import \
+    AttributeChangeTracker
 from application.use_cases.products.update_product.update_product_input_port import \
     UpdateProductInputPort
+from framework.web_scraper import coles_logic, woolworths_logic
 from framework.web_scraper.get_merchants_presenter import GetMerchantsPresenter
 from framework.web_scraper.get_products_presenter import GetProductsPresenter
+from framework.web_scraper.models import ScrapedProductOffer
+from framework.web_scraper.session import create_session
 from framework.web_scraper.update_product_presenter import \
     UpdateProductPresenter
 from interface_adaptors.controllers.merchant_controller import \
     MerchantController
 from interface_adaptors.controllers.product_controller import ProductController
-
-sys.path.append(os.getcwd())
-
-from typing import List
-
-from framework.web_scraper import coles_logic, woolworths_logic
-from framework.web_scraper.models import ScrapedProductOffer
-from framework.web_scraper.session import create_session
 
 # TODO: Make into use cases, this'll do for now though
 # TODO: Logging --- would be useful to know what the search term was that caused the error, and which merchant, etc
@@ -32,7 +28,8 @@ class WebScraper:
 
     def search_for_product(self, search_term: str, start_page: int = 1, limit_results_to: int = 10) -> List[ScrapedProductOffer]:
         with create_session() as _Session:
-            _Session.get('https://www.woolworths.com.au') # TODO: Why do .get() here?
+            _Session.get('https://www.woolworths.com.au')
+            # TODO: Why do .get() here? seems you get connection time out without it when making request inside .search()
             _WoolworthsProductOffers = [ScrapedProductOffer.translate_woolworths_offer(_ProductOffer)
                                         for _ProductOffer
                                         in woolworths_logic.search(_Session, search_term, start_page, start_page + 1, limit_results_to)]
@@ -44,7 +41,7 @@ class WebScraper:
 
             _ScrapedOffers = _WoolworthsProductOffers + _ColesProductOffers
             for _Offer in _ScrapedOffers:
-                _Offer.image = base64.b64encode(_Session.get(_Offer.image_uri).content)
+                _Offer.image = b64encode(_Session.get(_Offer.image_uri).content)
 
             return _ScrapedOffers
 
@@ -61,6 +58,7 @@ class WebScraper:
         await self.product_controller.get_products_async(_GetProductsPresenter)
 
         with create_session() as _Session:
+            _Session.get('https://www.woolworths.com.au')
             _WoolworthsMerchantID = next(_Merchant.merchant_id for _Merchant in _GetMerchantsPresenter.merchants if _Merchant.name == "Woolworths")
             _WoolworthsProductOffers = [
                 (_Product, ScrapedProductOffer.translate_woolworths_offer(woolworths_logic.get_by_stockcode(_Session, _Product.merchant_stockcode)))
@@ -69,9 +67,10 @@ class WebScraper:
                 if _Product.merchant_id == _WoolworthsMerchantID
             ]
 
+            _Session.get('https://www.coles.com.au/')
             _ColesMerchantID = next(_Merchant.merchant_id for _Merchant in _GetMerchantsPresenter.merchants if _Merchant.name == "Coles")
             _ColesProductOffers = [
-                (_Product, ScrapedProductOffer.translate_coles_offer(coles_logic.get_by_stockcode(_Session, _Product.merchant_stockcode)))
+                (_Product, ScrapedProductOffer.translate_coles_offer(coles_logic.get_by_stockcode(_Session, _Product.merchant_stockcode, _Product.name)))
                 for _Product
                 in _GetProductsPresenter.products
                 if _Product.merchant_id == _ColesMerchantID
@@ -80,24 +79,16 @@ class WebScraper:
             _UpdatedProducts: List[ProductDto] = []
             for _Product, _Offer in _WoolworthsProductOffers + _ColesProductOffers:
                 _InputPort = UpdateProductInputPort(
-                    is_available = _Offer.is_available,
-                    price_now = _Offer.price_now,
-                    price_was = _Offer.price_was,
+                    is_available = AttributeChangeTracker(_Offer.is_available),
+                    price_now = AttributeChangeTracker(_Offer.price_now),
+                    price_was = AttributeChangeTracker(_Offer.price_was),
                     product_id = _Product.product_id)
 
                 _Presenter = UpdateProductPresenter()
                 await self.product_controller.update_product_async(_InputPort, _Presenter)
                 _UpdatedProducts.append(_Presenter.result)
 
+            for _Product in _UpdatedProducts:
+                _Product.image = b64encode(_Product.image) # FIXME: THIS FEELS VERY WRONG, NEED TO REASSESS STORAGE AND RETRIEVAL OF IMAGES (could move encode to mapping)
+
             return _UpdatedProducts
-
-
-
-
-# if __name__ == "__main__":
-#     x = search_for_product("water")
-#     # x.sort(key=lambda x: x.price_now)
-#     g = Image.open(BytesIO(requests.get(x[0].image).content))
-#     h = requests.get(x[0].image).content
-#     print(type(h))
-#     v = 0

97cb4da51fa543e1250291dd19ff6b6859449d48
Ben <ben.talese@gmail.com>
2023-12-12 08:41:38 +1100

    Implement get product offers endpoint

--- framework/api/routes/web_scraper/web_scraper_router.py
+++ framework/api/routes/web_scraper/web_scraper_router.py
@@ -1,20 +1,34 @@
-from flask import Blueprint, jsonify, request
+from clapy import IServiceProvider
+from flask import Blueprint, current_app, jsonify, request
 
 from framework.api.routes.web_scraper.search_for_product_query import \
     SearchForProductQuery
+from framework.api.view_models.product_view_model import get_product_view_model
 from framework.api.view_models.scraped_product_offer_view_model import \
     get_scraped_product_offer_view_model
-from framework.web_scraper.controller import search_for_product
+from framework.web_scraper.controller import WebScraper
+from interface_adaptors.controllers.merchant_controller import \
+    MerchantController
+from interface_adaptors.controllers.product_controller import ProductController
 
 WEB_SCRAPER_ROUTER = Blueprint("WEB_SCRAPER_ROUTER", __name__, url_prefix="/api/webScraper")
 
+
+# TODO: Possibly just belongs on the product router?? why make this distinction (feels bad tbh, shows implementation details)
+# TODO Should use DI once web scraper is setup more clean, web scraper is a hosted service under the API
 @WEB_SCRAPER_ROUTER.route("/search", methods=["POST"])
 async def search_for_product_async():
-    # _ServiceProvider: IServiceProvider = current_app.service_provider
-    # _ProductController: ProductController = _ServiceProvider.get_service(ProductController)
-    # _Presenter: CreateProductPresenter = _ServiceProvider.get_service(CreateProductPresenter)
-    # await _ProductController.create_product_async(CreateProductInputPort(**request.request_object), _Presenter)
-    # return _Presenter.result
-    query: SearchForProductQuery = request.request_object
-    result = search_for_product(query.search_term, query.start_page)
-    return jsonify([get_scraped_product_offer_view_model(offer) for offer in result])
+    _ServiceProvider: IServiceProvider = current_app.service_provider
+    _ProductController: ProductController = _ServiceProvider.get_service(ProductController)
+    _MerchantController: MerchantController = _ServiceProvider.get_service(MerchantController)
+    _Query: SearchForProductQuery = request.request_object
+    _Result = WebScraper(_MerchantController, _ProductController).search_for_product(_Query.search_term, _Query.start_page)
+    return jsonify([get_scraped_product_offer_view_model(_Offer) for _Offer in _Result])
+
+@WEB_SCRAPER_ROUTER.route("/offers", methods=["GET"])
+async def get_product_offers_async():
+    _ServiceProvider: IServiceProvider = current_app.service_provider
+    _ProductController: ProductController = _ServiceProvider.get_service(ProductController)
+    _MerchantController: MerchantController = _ServiceProvider.get_service(MerchantController)
+    _Result = await WebScraper(_MerchantController, _ProductController).get_product_offers_async()
+    return jsonify([get_product_view_model(_Product) for _Product in _Result])

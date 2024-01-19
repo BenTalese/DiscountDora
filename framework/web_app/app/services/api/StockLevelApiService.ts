9c85e6f62316db0390a70c8a150f9c2bc7abbc8c
Ben <ben.talese@gmail.com>
2024-01-19 21:18:44 +1100

    Add stock levels api service

---
+++ framework/web_app/app/services/api/StockLevelApiService.ts
@@ -0,0 +1,13 @@
+import { StockLevel } from "@/models/StockLevel";
+import AxiosHttpClient from "./AxiosHttpClient";
+
+export default class StockLevelApiService {
+    private httpClient: AxiosHttpClient;
+
+    constructor() {
+        this.httpClient = new AxiosHttpClient();
+    }
+
+    getAll = async (): Promise<StockLevel[]> =>
+        await this.httpClient.get<StockLevel[]>('/stock-levels');
+}

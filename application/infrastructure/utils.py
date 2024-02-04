4a8f1494fc4c669cb53eef68e3428967f7f8d69d
Atif <atif.onat@studytafensw.edu.au>
2024-02-05 09:08:49 +1100

    fix get_attributes_ending_with in utils replacing an extra backslash for Windows environments

--- application/infrastructure/utils.py
+++ application/infrastructure/utils.py
@@ -36,7 +36,7 @@ def get_attributes_ending_with(term: str, path_to_search: str):
         Common.apply_exclusion_filter(_Directories, DIR_EXCLUSIONS)
         Common.apply_exclusion_filter(_Files, FILE_EXCLUSIONS)
 
-        _Namespace = _Root.replace('/', '.').replace('\\\\', '.').lstrip(".")
+        _Namespace = _Root.replace('/', '.').replace('\\', '.').lstrip(".")
         for _File in _Files:
             _Module = importlib.import_module(f"{_Namespace}.{_File[:-3]}", package=None)
             for _AttributeName, _AttributeValue in inspect.getmembers(_Module):

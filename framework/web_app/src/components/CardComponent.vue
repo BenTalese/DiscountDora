12d57bab99032362735087755d38f1e6e67f27c9
Atif <atif.onat@studytafensw.edu.au>
2024-02-23 08:39:42 +1100

    Add card component

---
+++ framework/web_app/src/components/CardComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <!-- TODO: Loader colour, match colour scheme -->
+    <q-card
+      bordered
+      class="rounded-borders text-white"
+      flat
+      style="height: 100%; display: flex; flex-direction: column;">
+      <q-img
+        class="bg-white"
+        img-class="q-pa-lg"
+        loading="lazy"
+        :src="img ?? 'https://shop.coles.com.au/wcsstore/Coles-CAS/images/1/9/8/198393-zm.jpg'"
+      >
+        <template v-slot:error>
+          <div class="absolute-full flex flex-center bg-negative text-white">
+            Panic
+          </div>
+        </template>
+      </q-img>
+
+      <div class="bg-blue-grey" style="display: flex; flex: 1; flex-direction: column;">
+
+        <q-card-section>
+          <q-btn
+          fab
+          color="white"
+          icon="favorite"
+          class="absolute text-grey"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+          />
+
+        </q-card-section>
+
+        <q-card-section class="bg-blue-grey q-pt-none">
+          <div class="col text-h7" style="flex: 1;">
+            {{ bodyMainText ?? 'Cosmic Zooper Dooper'}}
+          </div>
+          <div class="text-subtitle1">
+            ${{ bodySubtitleText ?? '5.99'}}
+          </div>
+        </q-card-section>
+
+        <q-card-actions align="right" class="bg-blue-grey">
+          <q-chip color="grey" :label="chipLabel ?? 'Coles' " text-color="white">
+          </q-chip>
+        </q-card-actions>
+
+      </div>
+
+    </q-card>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    // TODO: Figure what data is received when an image does not exist.
+    // Empty img should be replaced with a default/placeholder img.
+    bodyMainText: string
+    bodySubtitleText: string
+    chipLabel: string
+    img: string
+  }>()
+</script>

7da4c46948e47547e3cf9a31a03a4ae7ff82a0bd
Atif <atif.onat@studytafensw.edu.au>
2024-02-24 17:13:13 +1100

    add icon click event to card component

--- framework/web_app/src/components/CardComponent.vue
+++ framework/web_app/src/components/CardComponent.vue
@@ -9,6 +9,9 @@
         class="bg-white"
         img-class="q-pa-lg"
         loading="lazy"
+        spinner-color="primary"
+        spinner-size="2rem"
+        placeholder-src=""
         :src="img ?? 'https://shop.coles.com.au/wcsstore/Coles-CAS/images/1/9/8/198393-zm.jpg'"
       >
         <template v-slot:error>
@@ -22,17 +25,19 @@
 
         <q-card-section>
           <q-btn
-          fab
-          color="white"
-          icon="favorite"
-          class="absolute text-grey"
-          style="top: 0; right: 12px; transform: translateY(-50%);"
+            fab
+            color="white"
+            :icon="icon ?? 'favorite'"
+            class="absolute"
+            :class="iconClass"
+            style="top: 0; right: 12px; transform: translateY(-50%);"
+            @click="emit('icon-click')"
           />
 
         </q-card-section>
 
-        <q-card-section class="bg-blue-grey q-pt-none">
-          <div class="col text-h7" style="flex: 1;">
+        <q-card-section class="bg-blue-grey q-pt-none" style="flex: 1;">
+          <div class="col text-h7">
             {{ bodyMainText ?? 'Cosmic Zooper Dooper'}}
           </div>
           <div class="text-subtitle1">
@@ -51,12 +56,21 @@
 </template>
 
 <script setup lang="ts">
+  const emit = defineEmits<{
+    (e: 'icon-click'): void
+  }>()
+
   defineProps<{
     // TODO: Figure what data is received when an image does not exist.
     // Empty img should be replaced with a default/placeholder img.
     bodyMainText: string
     bodySubtitleText: string
     chipLabel: string
+    icon: string
+    iconClass: string
     img: string
   }>()
+
+  // TODO: Define defaults & remove conditional defaults from template.
+  // Ensure where appropriate the usage of props have default values being utilised, e.g., placeholder img
 </script>

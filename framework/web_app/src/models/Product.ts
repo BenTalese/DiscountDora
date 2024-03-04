b09dcb6628246f27027c840bd7178582e0abe9d4
Atif <atif.onat@studytafensw.edu.au>
2024-03-04 19:45:24 +1100

    rename product model for consistency

--- framework/web_app/src/models/Product.ts
+++ framework/web_app/src/models/Product.ts
@@ -2,7 +2,7 @@ export type Product = {
     brand: string
     image: string
     is_available: boolean
-    merchant: string
+    merchant_name: string
     merchant_stockcode: string
     name: string
     price_now: number

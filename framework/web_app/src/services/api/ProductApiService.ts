1a74d37bab48e093ab01fb7e43493af8896c1b23
Atif <atif.onat@studytafensw.edu.au>
2024-03-04 19:45:49 +1100

    add get all dapi products to service

--- framework/web_app/src/services/api/ProductApiService.ts
+++ framework/web_app/src/services/api/ProductApiService.ts
@@ -1,6 +1,7 @@
 import type { ScrapedProductOffer } from "src/models/ScrapedProductOffer";
 import type { CreatedResponse } from "./AxiosHttpClient";
 import AxiosHttpClient from "./AxiosHttpClient";
+import { Product } from "src/models/Product";
 
 export default class ProductApiService {
     private dapiHttpClient: AxiosHttpClient;
@@ -11,8 +12,13 @@ export default class ProductApiService {
         this.mapiHttpClient = new AxiosHttpClient(5172);
     }
 
+    // TODO: Need to decide how to deal with delete product
+    // Do we just want to archive/active-inactive in order continue tracking products
     create = async (command: CreateProductCommand): Promise<CreatedResponse> =>
         await this.dapiHttpClient.post<CreatedResponse>("/products", command);
+
+    getAll = async (): Promise<Product[]> =>
+        await this.dapiHttpClient.get<Product[]>('/products');
 
     searchByTerm = async (query: SearchByTermQuery): Promise<ScrapedProductOffer[]> =>
         await this.mapiHttpClient.get<ScrapedProductOffer[]>(`/products/search/${query.search_term}`);
@@ -25,7 +31,7 @@ export type SearchByTermQuery = {
 
 export type CreateProductCommand = {
     brand: string
-    image: string
+    image: string //TODO: Check if img is good before saving it, prob in use case
     is_available: boolean
     merchant_name: string
     merchant_stockcode: string

dd7c4ed0e7ca0afc9f9b3f6ea2fd4fc0d825872a
Atif <atif.onat@studytafensw.edu.au>
2024-03-04 19:45:04 +1100

    Add select component

---
+++ framework/web_app/src/components/SelectComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <q-select
+        bg-color="white"
+        class="q-ma-sm"
+        :clearable="clearable"
+        clear-icon="cancel"
+        dense
+        color="black"
+        dropdown-icon="expand_more"
+        :label="label"
+        label-color="black"
+        :multiple="multiple"
+        :options="options"
+        :option-label="optionLabel"
+        standout="bg-teal text-yellow"
+        style="min-width: 90px;"
+        v-model="internalModelValue"
+        @update:model-value="onUpdateModelValue"
+        transition-show="jump-up"
+        transition-hide="jump-up"
+    >
+        <template v-slot:no-option>
+            <q-item>
+                <q-item-section class="text-italic text-grey">
+                    No options
+                </q-item-section>
+            </q-item>
+        </template>
+    </q-select>
+</template>
+
+<script setup lang="ts">
+
+    const emit = defineEmits<{
+        (e: 'update:model-value', value: unknown) : void
+    }>();
+
+    const props = defineProps<{
+        clearable?: boolean
+        label: string
+        multiple?: boolean
+        options: Array<unknown>
+        optionLabel: ((option: string | unknown) => string) | string | undefined
+        modelValue: Array<unknown> | unknown
+    }>()
+
+    // const internalOptions = props.optionsSelector ?? ;
+    const internalModelValue = props.modelValue;
+
+    const onUpdateModelValue = (value: string | null): void =>
+        emit('update:model-value', value);
+</script>

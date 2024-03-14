ef2aefa712dbff250a3622244b3b76d66ef1210d
Atif <atif.onat@studytafensw.edu.au>
2024-03-15 08:43:56 +1100

    add filters to product search + refatoring

--- framework/web_app/src/pages/ProductSearch.vue
+++ framework/web_app/src/pages/ProductSearch.vue
@@ -1,65 +1,87 @@
 <template>
     <div class="row no-wrap q-pa-md" style="min-width: 100px; width: 100%;">
-        <q-input dense outlined square v-model="searchTerm" placeholder="Search" class="bg-white col">
+        <!-- TODO: Disable until results are rendered, expensive operation -->
+        <q-input
+            @keydown.enter="search"
+            dense outlined square v-model="searchTerm" placeholder="Search" class="bg-white col">
             <template v-slot:append>
                 <q-btn round dense flat icon="search" @click="search" />
                 <q-btn round dense flat icon="tune" @click="toggleFilter" />
             </template>
         </q-input>
     </div>
-        <!-- style="min-width: 100px; width: 100%;" -->
         <!-- TODO: research ordering convention of component props -->
         <!-- TODO: props position, loading -->
         <!-- TODO: Vue set prop to bool without binding, :persistent="false"-->
         <!-- Transition is likely redundant, may be worth Ben testing on Linux (doc says known windows issues) -->
+        <!-- loader -->
+        <!-- unsave product -->
         <Transition appear :duration="300"
             enter-active-class="animated fadeIn"
             leave-active-class="animated fadeOut">
-            <div class="row q-pa-sm bg-red" v-show="showFilter">
-                <DropdownComponent label="Store" :list-items="stores" />
-                <DropdownComponent label="In Stock" :list-items="stores" />
+            <div class="row q-pa-sm" v-show="showFilter">
+                <SelectComponent
+                    label="Stores"
+                    :icon-name="(isSelected: boolean): string => isSelected ? 'check_box' : 'check_box_outline_blank'"
+                    :multiple="true"
+                    :options="productStore.merchants ?? []"
+                    :option-label="(merchant: Merchant) => merchant.name"
+                    v-model="productStore.filterOptions.stores"
+                />
+                <SelectComponent
+                    label="Sort By"
+                    :options="ProductSearchSortByOptions"
+                    :option-label="optionLabelSelector"
+                    v-model="productStore.filterOptions.sortBy"
+                />
                 <q-btn
-                    class="bg-white q-ma-sm"
+                    class="q-ma-sm"
+                    :class="productStore.filterOptions.isAvailable ? 'bg-blue': 'bg-white'"
                     no-caps
                     no-wrap
                     size="md"
                     :stretch="false"
                     type="button"
+                    @click="() => productStore.toggleIsAvailableFilter()"
                 >
                     <template v-slot>
-                        Saved
+                        <span style="font-weight:400">In Stock</span>
+                        <!-- OG 500 WEIGHT -->
                     </template>
                 </q-btn>
-                <!-- <SelectComponent
-                    label="Stores"
-                    multiple
-                    :options="stores"
-                    :option-value="undefined"
-                    v-model="selectModel"
-                /> -->
+                <q-btn
+                    class="q-ma-sm"
+                    :class="productStore.filterOptions.isSaved ? 'bg-blue': 'bg-white'"
+                    no-caps
+                    no-wrap
+                    size="md"
+                    :stretch="false"
+                    type="button"
+                    text-color="black"
+                    @click="() => productStore.toggleIsSavedFilter()"
+                >
+                    <template v-slot>
+                        <span style="font-weight:400">Saved</span>
+                    </template>
+                </q-btn>
                 <!-- TODO: mock has 'specials', do we want this? should it be a filter rather than sort? -->
-                <SelectComponent
-                    label="Sort By"
-                    :options="ProductSearchSortOptions"
-                    :option-label="optionLabelSelector"
-                    v-model="productStore.activeSortOption"
-                />
+
                 <!-- TODO: Binding the state directly to components which modify it may be bad -->
             </div>
         </Transition>
 
+        <!-- TODO: PRICE NOW comes as null sometimes, encountered in Woolies response -->
     <div style="display: grid; grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));">
-        <!-- TODO: lighter shade of red on saved heart icon -->
-        <!-- TODO: Add offer.brand back in, not all names contain the brand -->
+        <!-- TODO: COLES, Add offer.brand back in, not all names contain the brand -->
             <div
             class="q-pa-md"
-            v-for="offer in productStore.sortedProductOffers" :key="offer.merchant_stockcode">
+            v-for="offer in productStore.filteredProductOffers" :key="offer.merchant_stockcode">
             <CardComponent
-                :body-main-text="offer.name"
-                :body-subtitle-text="offer.price_now.toFixed(2).toString()"
+                :body-main-text="getOfferFullName(offer.brand, offer.name)"
+                :body-subtitle-text="offer.price_now?.toFixed(2).toString()"
                 :chip-label="offer.merchant"
                 icon="favorite"
-                :icon-class="isSavedProduct(offer) ? 'text-red' : 'text-grey'"
+                :icon-class="isSavedProduct(offer) ? 'text-red-12' : 'text-grey'"
                 :img="imageService.decodeBase64Image(offer.image)"
                 @icon-click="productStore.saveProduct(offer)"
             />
@@ -69,15 +91,15 @@
 
 <script setup lang="ts">
 import CardComponent from 'src/components/CardComponent.vue';
-import DropdownComponent from 'src/components/DropdownComponent.vue';
 import SelectComponent from 'src/components/SelectComponent.vue';
-import { ProductSearchSortOption } from 'src/helpers/ProductSearchSortOption';
-import { ProductSearchSortOptions } from 'src/helpers/ProductSearchSortOptions';
+import { ProductSearchSortByOption } from 'src/helpers/ProductSearchSortByOption';
+import { ProductSearchSortByOptions } from 'src/helpers/ProductSearchSortByOptions';
 import { Product } from 'src/models/Product';
+import { Merchant } from 'src/models/Merchant';
 import { ScrapedProductOffer } from 'src/models/ScrapedProductOffer';
 import ImageService from 'src/services/files/ImageService';
 import { useProductStore } from 'src/stores/ProductStore';
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 
 const imageService = new ImageService();
 const productStore = useProductStore();
@@ -88,13 +110,16 @@ const showFilter = ref(true);
 let currentPage = 1;
 
 // TODO: Need somewhere to load initial states. Could... load it just in this component...maybe
-productStore.getProducts()
+productStore.getProducts();
+productStore.getMerchants();
 
-// TODO: revise == vs ===
+const getOfferFullName = (brand: string, name: string): string =>
+    name.includes(brand) ? name : `${brand} ${name}`;
+
 const isSavedProduct = (offer: ScrapedProductOffer): boolean => {
-    let savedProduct = productStore.savedProducts?.find((product: Product) =>
+    let savedProduct = productStore.products?.find((product: Product) =>
         product.merchant_stockcode == offer.merchant_stockcode &&
-        product.merchant_name == offer.merchant)
+        product.merchant_name === offer.merchant)
 
     return savedProduct != undefined;
 };
@@ -104,11 +129,7 @@ const search = (): Array<ScrapedProductOffer> => productStore.searchByTerm({
     start_page: currentPage
 }) as Array<ScrapedProductOffer>;
 
-const stores = computed((): Array<string> => {
-    return [...new Set(productStore.scrapedProductOffers?.map(offer => offer.merchant))].sort()
-});
-
-const optionLabelSelector = (option: ProductSearchSortOption) => {
+const optionLabelSelector = (option: ProductSearchSortByOption) => {
     return option.Description;
 };
 

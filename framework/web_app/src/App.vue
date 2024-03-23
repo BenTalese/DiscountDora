7062607a74b38de9637991aacaa9676b0f1194f1
Atif <atif.onat@studytafensw.edu.au>
2024-03-24 00:44:19 +1100

    Move initial app wide loads to app component

--- framework/web_app/src/App.vue
+++ framework/web_app/src/App.vue
@@ -3,5 +3,16 @@
 </template>
 
 <script setup lang="ts">
+import { useProductStore } from './stores/ProductStore';
+
+  const productStore = useProductStore();
+
+  //TODO: If initial load up fails.
+  // E.g., user has no connection, fetch fails then user gets connection but no re-fetch
+  //  (no data for some components, partially working pages)
+  // Solution is likely to conditionally re-fetch in the store as Ben suggested
+  // or to re fetch on pages that require it if there is no data
+  productStore.getMerchants();
+  productStore.getProducts();
 
 </script>

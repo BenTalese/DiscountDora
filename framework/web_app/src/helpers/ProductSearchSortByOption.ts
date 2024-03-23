c9cf440c3f6f4b420c44a322b4833e86adc379a5
Atif <atif.onat@studytafensw.edu.au>
2024-03-24 00:45:11 +1100

    remove legacy sort by options

--- framework/web_app/src/helpers/ProductSearchSortByOption.ts
+++
@@ -1,7 +0,0 @@
-import { ScrapedProductOffer } from 'src/models/ScrapedProductOffer';
-
-
-export interface ProductSearchSortByOption {
-    Description: string;
-    Apply: (offers: ScrapedProductOffer[]) => ScrapedProductOffer[];
-}

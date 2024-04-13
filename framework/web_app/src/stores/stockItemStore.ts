5f5c610a5e972bf68b64de004393d9ba74a60b07
Ben <ben.talese@gmail.com>
2024-04-13 17:51:09 +1000

    Add create and update to stock item store

--- framework/web_app/src/stores/stockItemStore.ts
+++ framework/web_app/src/stores/stockItemStore.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia';
 import type { StockItem } from 'src/models/StockItem';
-import StockItemApiService from 'src/services/api/StockItemApiService';
+import StockItemApiService, { CreateStockItemCommand } from 'src/services/api/StockItemApiService';
 import { Ref, ref } from 'vue';
 
 const stockItemApiService = new StockItemApiService();
@@ -12,9 +12,21 @@ export const useStockItemStore = defineStore('stockItem', () => {
     // Getters
 
     // Actions
-    const getStockItems = async () => await stockItemApiService
-        .getAll()
+    const getStockItemsAsync = async () => await stockItemApiService
+        .getAllAsync()
         .then((res) => stockItems.value = res)
 
-    return { stockItems, getStockItems }
+    const createStockItemAsync = async (stockItemToCreate: CreateStockItemCommand) => await stockItemApiService
+        .createAsync(stockItemToCreate)
+        .then(async (res) => stockItems.value.push(await stockItemApiService.getAsync(res.id)))
+
+    const updateStockLevelAsync = async (stockItemID: string, stockLevelID: string) => await stockItemApiService
+        .updateAsync(stockItemID, { stock_level_id: stockLevelID })
+        .then(async () => {
+            // TODO: Loading
+            const stockItemIndex = stockItems.value.findIndex(si => si.stock_item_id == stockItemID);
+            stockItems.value[stockItemIndex] = await stockItemApiService.getAsync(stockItemID);
+        })
+
+    return { stockItems, getStockItemsAsync, createStockItemAsync, updateStockLevelAsync }
 });

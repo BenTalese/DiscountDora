53409d7765fe6c19dfa3e66d2efb6b64700b54c1
Atif <atif.onat@studytafensw.edu.au>
2024-04-15 07:52:37 +1000

    Add documentation to card component props & emits

--- framework/web_app/src/components/CardComponent.vue
+++ framework/web_app/src/components/CardComponent.vue
@@ -47,12 +47,44 @@
   //#region Props & Emits
 
   interface ICardComponentProps {
+
+    /**
+     * The text displayed in the body of the card;
+     * Utilise the body slot to customise the body section as a whole;
+     * Recommended component for slot: QCardSection;
+     */
     body?: string
-    chipLabel?: string
-    chipColour?: string
-    icon: string
-    iconClass: string
-    img?: string
+
+    /**
+     * The label of the quasar chip component in the card footer;
+     * Only rendered when a value is supplied;
+     */
+    chipLabel?: string | undefined
+
+    /**
+     * The colour applied to the chip in the card footer;
+     * Only applied if the chipLabel prop has a value;
+     * Default: grey;
+     */
+    chipColour?: string | undefined
+
+
+    /**
+     * Icon name following Quasar convention; Make sure you have the icon library installed unless you are using 'img:' prefix; If 'none' (String) is used as value then no icon is rendered (but screen real estate will still be used for it);
+     */
+    icon?: string | undefined
+
+    /**
+     * The CSS classes applied to card component's icon;
+     */
+    iconClass?: string | undefined
+
+    /**
+     * Path to image;
+     * Default: '../../src/assets/dora-logo.png';
+     */
+    img?: string | undefined
+
   }
 
   withDefaults(defineProps<ICardComponentProps>(), {
@@ -61,7 +93,12 @@
   })
 
   const emit = defineEmits<{
+
+    /**
+     * Emitted when the click event is triggered on the card component's icon;
+     */
     (e: 'icon-click'): void
+
   }>()
 
   //#endregion Props & Emits

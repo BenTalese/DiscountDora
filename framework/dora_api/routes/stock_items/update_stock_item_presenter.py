7ee38cada77152f8b48d92a7642c6603b322e938
Ben <ben.talese@gmail.com>
2024-04-23 08:50:39 +1000

    Fix not found presenter methods

--- framework/dora_api/routes/stock_items/update_stock_item_presenter.py
+++ framework/dora_api/routes/stock_items/update_stock_item_presenter.py
@@ -5,6 +5,9 @@ from application.dtos.stock_item_dto import StockItemDto
 from application.services.ipersistence_context import IPersistenceContext
 from application.use_cases.stock_items.update_stock_item.iupdate_stock_item_output_port import IUpdateStockItemOutputPort
 from domain.entities.base_entity import EntityID
+from domain.entities.stock_item import StockItem
+from domain.entities.stock_level import StockLevel
+from domain.entities.stock_location import StockLocation
 from framework.dora_api.infrastructure.base_presenter import BasePresenter
 
 
@@ -14,13 +17,13 @@ class UpdateStockItemPresenter(BasePresenter, IUpdateStockItemOutputPort):
         self.persistence = persistence
 
     async def present_stock_item_not_found_async(self, stock_item_id: EntityID):
-        await self.entity_existence_failure_async(nameof(stock_item_id), stock_item_id)
+        await self.entity_existence_failure_async(nameof(StockItem), nameof(stock_item_id), stock_item_id.value)
 
     async def present_stock_level_not_found_async(self, stock_level_id: EntityID):
-        await self.entity_existence_failure_async(nameof(stock_level_id), stock_level_id.value)
+        await self.entity_existence_failure_async(nameof(StockLevel), nameof(stock_level_id), stock_level_id.value)
 
     async def present_stock_location_not_found_async(self, stock_location_id: EntityID):
-        await self.entity_existence_failure_async(nameof(stock_location_id), stock_location_id.value)
+        await self.entity_existence_failure_async(nameof(StockLocation), nameof(stock_location_id), stock_location_id.value)
 
     async def present_stock_item_updated_async(self, stock_item: StockItemDto):
         await self.persistence.save_changes_async()

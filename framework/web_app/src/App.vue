a78acab9315f26bb90b80ebd11447b20f5778ef0
Atif <atif.onat@studytafensw.edu.au>
2024-04-24 14:32:57 +1000

    Move initial app wide fetches to a beforeMount lifecycle hook

--- framework/web_app/src/App.vue
+++ framework/web_app/src/App.vue
@@ -3,18 +3,16 @@
 </template>
 
 <script setup lang="ts">
+import { onBeforeMount } from 'vue';
 import { useMerchantStore } from './stores/MerchantStore';
 import { useProductStore } from './stores/ProductStore';
 
   const merchantStore = useMerchantStore();
   const productStore = useProductStore();
 
-  //TODO: If initial load up fails.
-  // E.g., user has no connection, fetch fails then user gets connection but no re-fetch
-  //  (no data for some components, partially working pages)
-  // Solution is likely to conditionally re-fetch in the store as Ben suggested
-  // or to re fetch on pages that require it if there is no data
-  merchantStore.getMerchantsAsync();
-  productStore.getProductsAsync();
+  onBeforeMount(() => {
+    merchantStore.getMerchantsAsync();
+    productStore.getProductsAsync();
+  })
 
 </script>

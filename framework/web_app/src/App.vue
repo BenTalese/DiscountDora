27e6edabc6b25b75c2b74a3c02280807a72fc9f1
Atif <atif.onat@studytafensw.edu.au>
2024-04-25 11:42:29 +1000

    remove redundant onBeforeMount hook

--- framework/web_app/src/App.vue
+++ framework/web_app/src/App.vue
@@ -3,16 +3,13 @@
 </template>
 
 <script setup lang="ts">
-import { onBeforeMount } from 'vue';
 import { useMerchantStore } from './stores/MerchantStore';
 import { useProductStore } from './stores/ProductStore';
 
   const merchantStore = useMerchantStore();
   const productStore = useProductStore();
 
-  onBeforeMount(() => {
-    merchantStore.getMerchantsAsync();
-    productStore.getProductsAsync();
-  })
+  merchantStore.getMerchantsAsync();
+  productStore.getProductsAsync();
 
 </script>

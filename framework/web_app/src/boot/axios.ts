2d07c0c287900b90a6d441eb706dd7a3757719f9
Ben <ben.talese@gmail.com>
2024-04-30 19:48:57 +1000

    Formatting

--- framework/web_app/src/boot/axios.ts
+++ framework/web_app/src/boot/axios.ts
@@ -1,11 +1,11 @@
-import { boot } from 'quasar/wrappers';
 import axios, { AxiosInstance } from 'axios';
+import { boot } from 'quasar/wrappers';
 
 declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $axios: AxiosInstance;
-    $api: AxiosInstance;
-  }
+    interface ComponentCustomProperties {
+        $axios: AxiosInstance;
+        $api: AxiosInstance;
+    }
 }
 
 // Be careful when using SSR for cross-request state pollution
@@ -17,15 +17,15 @@ declare module '@vue/runtime-core' {
 const api = axios.create({ baseURL: 'https://api.example.com' });
 
 export default boot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
+    // for use inside Vue files (Options API) through this.$axios and this.$api
 
-  app.config.globalProperties.$axios = axios;
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
+    app.config.globalProperties.$axios = axios;
+    // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
+    //       so you won't necessarily have to import axios in each vue file
 
-  app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
+    app.config.globalProperties.$api = api;
+    // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
+    //       so you can easily perform requests against your app's API
 });
 
 export { api };
--- framework/web_app/src/boot/i18n.ts
+++ framework/web_app/src/boot/i18n.ts
@@ -10,24 +10,24 @@ export type MessageSchema = typeof messages['en-US'];
 // See https://vue-i18n.intlify.dev/guide/advanced/typescript.html#global-resource-schema-type-definition
 /* eslint-disable @typescript-eslint/no-empty-interface */
 declare module 'vue-i18n' {
-  // define the locale messages schema
-  export interface DefineLocaleMessage extends MessageSchema {}
+    // define the locale messages schema
+    export interface DefineLocaleMessage extends MessageSchema { }
 
-  // define the datetime format schema
-  export interface DefineDateTimeFormat {}
+    // define the datetime format schema
+    export interface DefineDateTimeFormat { }
 
-  // define the number format schema
-  export interface DefineNumberFormat {}
+    // define the number format schema
+    export interface DefineNumberFormat { }
 }
 /* eslint-enable @typescript-eslint/no-empty-interface */
 
 export default boot(({ app }) => {
-  const i18n = createI18n({
-    locale: 'en-US',
-    legacy: false,
-    messages,
-  });
+    const i18n = createI18n({
+        locale: 'en-US',
+        legacy: false,
+        messages,
+    });
 
-  // Set i18n instance on app
-  app.use(i18n);
+    // Set i18n instance on app
+    app.use(i18n);
 });

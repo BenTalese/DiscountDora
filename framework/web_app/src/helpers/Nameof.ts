1f5ae6f41567d70e801006b661c2ac87b0d1444f
Atif <atif.onat@studytafensw.edu.au>
2024-05-04 06:49:39 +1000

    refactor nameof method, still has type safety

--- framework/web_app/src/helpers/Nameof.ts
+++ framework/web_app/src/helpers/Nameof.ts
@@ -1,21 +1,4 @@
-import NullArgumentError from '../exceptions/NullArgumentError';
-import NotImplementedError from '../exceptions/NotImplementedError';
+const nameof = <T>(key: keyof T): string =>
+    key.toString();
 
-/**
- * Retrieves the name of a property or method from a given selector function.
- * @param selector A function that selects a property or method from an object.
- * @returns The name of the selected property or method as a string.
- */
-export const nameof = <TObject>(selector: (obj: TObject) => unknown): string => {
-
-    if (!selector)
-        throw new NullArgumentError('Selector');
-
-    const selectorParts = selector.toString().split('.');
-
-    if (selectorParts.length > 1)
-        return selectorParts[selectorParts.length - 1];
-
-    else
-        throw new NotImplementedError('NameOf is only implemented for instance members, not for the instance itself. ');
-};
+export default nameof;

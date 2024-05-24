df66976f88db3892adfb23c70d12b83d1332a1aa
Ben <ben.talese@gmail.com>
2024-05-24 19:51:21 +1000

    Fetch initial app data

--- framework/web_app/src/App.vue
+++ framework/web_app/src/App.vue
@@ -3,15 +3,20 @@
 </template>
 
 <script setup lang="ts">
-import { useMerchantStore } from './stores/MerchantStore';
-import { useProductStore } from './stores/ProductStore';
+    import { useMerchantStore } from './stores/MerchantStore'
+    import { useProductStore } from './stores/ProductStore'
+    import { useStockItemStore } from './stores/stockItemStore'
+    import { useStockLevelStore } from './stores/stockLevelStore'
 
-// TODO: Maybe want to move these to boot files too?
-// TODO: Check if API online and report online/offline in UI?
-const merchantStore = useMerchantStore();
-const productStore = useProductStore();
+    // TODO: Maybe want to move these to boot files too?
+    // TODO: Check if API online and report online/offline in UI?
+    const merchantStore = useMerchantStore()
+    const productStore = useProductStore()
+    const stockItemStore = useStockItemStore()
+    const stockLevelStore = useStockLevelStore()
 
-merchantStore.getMerchantsAsync();
-productStore.getProductsAsync();
-
+    await merchantStore.getMerchantsAsync()
+    await productStore.getProductsAsync()
+    await stockItemStore.getStockItemsAsync()
+    await stockLevelStore.getStockLevelsAsync()
 </script>

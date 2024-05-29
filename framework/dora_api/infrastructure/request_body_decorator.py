04838c50daa5bb18686bdebac303a6bbc52d2bf7
Ben <ben.talese@gmail.com>
2024-05-29 21:17:09 +1000

    Update decorator descriptions

--- framework/dora_api/infrastructure/request_body_decorator.py
+++ framework/dora_api/infrastructure/request_body_decorator.py
@@ -5,6 +5,8 @@ def has_request_body(endpoint_name, request_body_class):
     `has_request_body` is a decorator that registers an endpoint name against a request body class,
     the purpose of which is to provide the expected request body to the deserialisation middleware
     for incoming web requests.
+
+    Using this decorator indicates this endpoint has a request body.
     """
     def decorator(func):
         REQUEST_BODYS_BY_ENDPOINT[endpoint_name] = request_body_class

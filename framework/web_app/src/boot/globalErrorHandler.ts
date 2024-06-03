dfd2438a76e06a82e5337298eeea840fe373f43a
Ben <ben.talese@gmail.com>
2024-06-03 10:31:39 +1000

    Put back event param so the global handler is hit

--- framework/web_app/src/boot/globalErrorHandler.ts
+++ framework/web_app/src/boot/globalErrorHandler.ts
@@ -13,6 +13,6 @@ window.onerror = function (msg, url, line, col, error) {
     Notify.create({ type: 'oopsie' });
 }
 
-window.addEventListener('unhandledrejection', function () {
+window.addEventListener('unhandledrejection', function (event) {
     Notify.create({ type: 'oopsie' });
 });

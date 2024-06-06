d2f759e832df1f3814760f3e44599627f68caef5
Ben <ben.talese@gmail.com>
2024-06-06 14:17:56 +1000

    Make front end wait for back end gracefully
    No more errors on startup!

--- framework/web_app/src/boot/stores.ts
+++ framework/web_app/src/boot/stores.ts
@@ -1,14 +1,25 @@
+import HealthApiService from 'src/services/api/healthApiService'
 import { useMerchantStore } from '../stores/MerchantStore'
 import { useProductStore } from '../stores/ProductStore'
 import { useStockItemStore } from '../stores/stockItemStore'
 import { useStockLevelStore } from '../stores/stockLevelStore'
 
-const merchantStore = useMerchantStore()
-const productStore = useProductStore()
-const stockItemStore = useStockItemStore()
-const stockLevelStore = useStockLevelStore()
+async function waitForApiStartupAsync() {
+    const healthApiService = new HealthApiService();
+    while (true) {
+        if (await healthApiService.healthCheckAsync()) {
+            break;
+        }
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+}
 
-merchantStore.getMerchantsAsync()
-productStore.getProductsAsync()
-stockItemStore.getStockItemsAsync()
-stockLevelStore.getStockLevelsAsync()
+async function initialiseStoresAsync() {
+    await useMerchantStore().getMerchantsAsync()
+    await useProductStore().getProductsAsync()
+    await useStockItemStore().getStockItemsAsync()
+    await useStockLevelStore().getStockLevelsAsync()
+}
+
+await waitForApiStartupAsync()
+await initialiseStoresAsync()

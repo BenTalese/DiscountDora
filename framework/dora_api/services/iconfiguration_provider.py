5637e462939a7f531f589f2932c85f7e7094ad7e
Ben <ben.talese@gmail.com>
2024-06-27 14:32:34 +1000

    Add configuration providers for DAPI and MAPI

---
+++ framework/dora_api/services/iconfiguration_provider.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+
+class IConfigurationProvider(ABC):
+
+    @abstractmethod
+    def get_api_host(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_api_port(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_db_connection_string(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_web_app_host(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_web_app_port(self) -> int:
+        pass
+
+    @abstractmethod
+    def is_debug_mode_enabled(self) -> bool:
+        pass
+
+    @abstractmethod
+    def is_modification_tracking_enabled(self) -> bool:
+        pass
+
+    @abstractmethod
+    def is_reloader_enabled(self) -> bool:
+        pass
---
+++ framework/merchant_api/infrastructure/configuration_provider.py
@@ -0,0 +1,47 @@
+import json
+import os
+from pathlib import Path
+
+from pydantic import BaseModel
+
+from framework.merchant_api.services.iconfiguration_provider import \
+    IConfigurationProvider
+
+
+class Config(BaseModel):
+    API_HOST: str
+    API_PORT: int
+    CHROMEDRIVER_PATH: str
+    DEBUG: bool
+    IGA_STORE_ID: int
+    USE_RELOADER: bool
+    WEB_APP_HOST: str
+    WEB_APP_PORT: int
+
+
+class ConfigurationProvider(IConfigurationProvider):
+    _configuration: Config
+
+    def __init__(self):
+        basedir = Path(__file__).parent.parent
+        with open(os.path.join(basedir, 'appsettings.json'), 'r') as _AppSettings:
+            self._configuration = Config(**json.load(_AppSettings))
+
+    def get_api_host(self) -> str:
+        return self._configuration.API_HOST
+
+    def get_api_port(self) -> int:
+        return self._configuration.API_PORT
+
+    def get_web_app_host(self) -> str:
+        return self._configuration.WEB_APP_HOST
+
+    def get_web_app_port(self) -> int:
+        return self._configuration.WEB_APP_PORT
+
+    def is_debug_mode_enabled(self) -> bool:
+        return self._configuration.DEBUG
+
+    def is_reloader_enabled(self) -> bool:
+        return self._configuration.USE_RELOADER
+

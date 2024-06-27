5b753da0429a9fe9793277391c790073459054b9
Ben <ben.talese@gmail.com>
2024-06-27 14:32:01 +1000

    Move service collection to infrastructure folder

--- framework/dora_api/infrastructure/service_collection_builder.py
+++ framework/dora_api/infrastructure/service_collection_builder.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from clapy import DependencyInjectorServiceProvider
 from dependency_injector import providers
@@ -7,6 +8,10 @@ from application.infrastructure.configure_services import \
     configure_application_services
 from application.infrastructure.utils import get_classes_ending_with
 from domain.infrastructure.configure_services import configure_domain_services
+from framework.dora_api.infrastructure.configuration_provider import \
+    ConfigurationProvider
+from framework.dora_api.services.iconfiguration_provider import \
+    IConfigurationProvider
 from framework.persistence.infrastructure.configure_services import \
     configure_persistence_services
 from interface_adaptors.infrastructure.configure_services import \
@@ -19,24 +24,28 @@ class ServiceCollectionBuilder:
 
     def build_service_provider(self):
         return self \
+            .register_configuration_provider() \
             .configure_persistence_services() \
             .configure_core_services() \
             .register_api_presenters() \
             .service_provider
 
-    def register_api_presenters(self):
-        for _Presenter in get_classes_ending_with('presenter', os.path.normpath('framework/dora_api/routes')):
-            self.service_provider.register_service(providers.Factory, _Presenter)
-
-        return self
-
     def configure_core_services(self):
         configure_domain_services(self.service_provider)
         configure_application_services(self.service_provider)
-        self.service_provider.configure_clapy_services([os.path.normpath('application/use_cases')], [r"venv", r"src"], [r".*main\.py"])
+        self.service_provider.configure_clapy_services([Path() / 'application' / 'use_cases'], [r"venv", r"src"], [r".*main\.py"])
         configure_interface_adaptors_services(self.service_provider)
         return self
 
     def configure_persistence_services(self):
         configure_persistence_services(self.service_provider)
+        return self
+
+    def register_api_presenters(self):
+        for _Presenter in get_classes_ending_with('presenter', Path() / 'framework' / 'dora_api' / 'routes'):
+            self.service_provider.register_service(providers.Factory, _Presenter)
+        return self
+
+    def register_configuration_provider(self):
+        self.service_provider.register_service(providers.Singleton, ConfigurationProvider, IConfigurationProvider)
         return self

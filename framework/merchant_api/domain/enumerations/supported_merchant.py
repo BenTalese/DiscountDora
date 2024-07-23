dab18b8de276d3063c6c5bb1f93fa3743ce59dc5
Ben <ben.talese@gmail.com>
2024-07-23 20:43:34 +1000

    Add aldi and iga to supported merchants

--- framework/merchant_api/domain/enumerations/supported_merchant.py
+++ framework/merchant_api/domain/enumerations/supported_merchant.py
@@ -3,5 +3,7 @@ from enum import Enum
 
 class SupportedMerchant(Enum):
     '''The supported merchants of the merchant API.'''
+    ALDI = "ALDI"
     COLES = "Coles"
+    IGA = "IGA"
     WOOLWORTHS = "Woolworths"

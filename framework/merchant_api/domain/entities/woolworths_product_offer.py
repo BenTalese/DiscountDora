868c7a429a1f82fb93cdf86ed4882278b323cb6d
Ben <ben.talese@gmail.com>
2024-07-23 20:43:20 +1000

    Fix flake errors

--- framework/merchant_api/domain/entities/woolworths_product_offer.py
+++ framework/merchant_api/domain/entities/woolworths_product_offer.py
@@ -2,16 +2,16 @@ from pydantic import BaseModel
 
 
 class WoolworthsProductOffer(BaseModel, extra='allow'):
-    Brand: str # 'Lindt'
-    CupString: str # '$0.80 / 100G'
+    Brand: str  # 'Lindt'
+    CupString: str  # '$0.80 / 100G'
     IsAvailable: bool
     InstoreCupString: str
     InstoreIsAvailable: bool
     InstorePrice: float
     InstoreWasPrice: float
-    LargeImageFile: str # 'https://cdn0.woolworths.media/content/wowproductimages/large/114682.jpg'
-    Name: str # 'Lindt Lindor Milk Chocolate Balls'
-    PackageSize: str # '333G'
+    LargeImageFile: str  # 'https://cdn0.woolworths.media/content/wowproductimages/large/114682.jpg'
+    Name: str  # 'Lindt Lindor Milk Chocolate Balls'
+    PackageSize: str  # '333G'
     Price: float
     Stockcode: int
     WasPrice: float

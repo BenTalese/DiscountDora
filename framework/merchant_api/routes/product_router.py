d043c597861608f8191761207a59259a6595b51e
Ben <ben.talese@gmail.com>
2024-07-26 21:14:57 +1000

    Refactoring and tidy up

--- framework/merchant_api/routes/product_router.py
+++ framework/merchant_api/routes/product_router.py
@@ -2,32 +2,24 @@ import logging
 import random
 import time
 from base64 import b64encode
-from itertools import groupby
 from typing import Dict, List
 from uuid import UUID
+from pydantic import BaseModel
 
 import requests
 from clapy import IServiceProvider
-from flask import Blueprint, current_app, jsonify
+from flask import Blueprint, current_app, jsonify, request
 
 from framework.merchant_api.domain.entities.dora_product import DoraProduct
 from framework.merchant_api.domain.entities.scraped_product_offer import \
     ScrapedProductOffer
-from framework.merchant_api.infrastructure.merchant_data_providers.coles_provider import \
-    ColesProvider
-from framework.merchant_api.infrastructure.merchant_data_providers.grocerize_provider import \
-    GrocerizeProvider
-from framework.merchant_api.infrastructure.merchant_data_providers.iga_provider import \
-    IGAProvider
-from framework.merchant_api.infrastructure.merchant_data_providers.save_on_groceries_provider import \
-    SaveOnGroceriesProvider
-from framework.merchant_api.infrastructure.merchant_data_providers.woolworths_provider import \
-    WoolworthsProvider
+from framework.merchant_api.domain.enumerations.supported_merchant import \
+    SupportedMerchant
+from framework.merchant_api.infrastructure.merchant_data_providers import \
+    get_healthy_merchant_data_providers
 from framework.merchant_api.infrastructure.session import get_cached_session
 from framework.merchant_api.services.iconfiguration_manager import \
     IConfigurationManager
-from framework.merchant_api.services.imerchant_data_provider import \
-    IMerchantDataProvider
 
 PRODUCT_ROUTER = Blueprint("PRODUCT_ROUTER", __name__, url_prefix="/api/products")
 
@@ -45,7 +37,8 @@ On navigation to product search, if nothing is configured open a modal to config
 
 SEPARATE SETTINGS PAGE (Atif idea)
 '''
-# TODO: Need to more closely inspect items such as fruit and veg for pricing information, see IGA uses whole price, also sometimes it's an "each" pricing
+# TODO: Need to more closely inspect items such as fruit and veg for pricing information,
+# see IGA uses whole price, also sometimes it's an "each" pricing
 
 
 @PRODUCT_ROUTER.route("/search", methods = ["POST"])
@@ -60,28 +53,23 @@ async def search_for_product_async() -> List[ScrapedProductOffer]:
     _ServiceProvider: IServiceProvider = current_app.service_provider
     _ConfigurationManager: IConfigurationManager = _ServiceProvider.get_service(IConfigurationManager)
     _Logger: logging.Logger = _ServiceProvider.get_service(logging.Logger)
-
+    _DataProviders = get_healthy_merchant_data_providers()
     _ScrapedOffers: List[ScrapedProductOffer] = []
 
-    # TODO: Move to method "get_healthy_providers"
-    _DataProviders: List[IMerchantDataProvider] = [
-        _ServiceProvider.get_service(ColesProvider),
-        _ServiceProvider.get_service(IGAProvider),
-        _ServiceProvider.get_service(GrocerizeProvider),
-        _ServiceProvider.get_service(SaveOnGroceriesProvider),
-        _ServiceProvider.get_service(WoolworthsProvider)
+    _MerchantsToSearch = [
+        _Merchant
+        for _Merchant
+        in _ConfigurationManager.get_enabled_merchants()
+        if _Merchant.name.value in _RequestBody.merchants_to_search
     ]
 
-    _HealthyDataProviders = [_Provider for _Provider in _DataProviders if _Provider.is_healthy]
-    _HealthyDataProviders.sort(key = lambda mdp: mdp.priority)
+    for _Merchant in _MerchantsToSearch:
 
-    for _Merchant in _ConfigurationManager.get_enabled_merchants():
-        for _MerchantDataProvider in _HealthyDataProviders:
+        for _MerchantDataProvider in _DataProviders:
 
             if not _MerchantDataProvider.is_merchant_supported(_Merchant):
                 continue
 
-            time.sleep(random.uniform(0, 2))
             try:
                 if _Offers := _MerchantDataProvider.search_by_term(_RequestBody.search_term, _Merchant, _RequestBody.result_limit):
                     _ScrapedOffers.extend(_Offers)
@@ -93,8 +81,14 @@ async def search_for_product_async() -> List[ScrapedProductOffer]:
                                   f" Search term: {_RequestBody.search_term}. Merchant: {_Merchant.name.value}", e)
 
     with get_cached_session() as _Session:
-        for _Offer in _ScrapedOffers:
-            _Offer.image = b64encode(_Session.get(_Offer.image_uri).content).decode('utf-8')
+        try:
+            for _Offer in _ScrapedOffers:
+                if _Offer.image_uri:
+                    _Offer.image = b64encode(_Session.get(_Offer.image_uri).content).decode('utf-8')
+
+        except Exception as e:
+            _Logger.exception(f"Encountered a problem grabbing image for product '{_Offer.name}' for merchant"
+                              f" '{_Offer.merchant_name}' with image URI: {_Offer.image_uri}", e)
 
     return jsonify(_ScrapedOffers)
 
@@ -103,48 +97,42 @@ async def search_for_product_async() -> List[ScrapedProductOffer]:
 async def get_product_offers_async():
     _ServiceProvider: IServiceProvider = current_app.service_provider
     _Logger: logging.Logger = _ServiceProvider.get_service(logging.Logger)
+    _DataProviders = get_healthy_merchant_data_providers()
 
     # TODO: Should MAPI be getting dora products and saving to them, or should the calling code of MAPI be responsible for this?
-    _SavedProducts = sorted(
-        (DoraProduct(**_Product) for _Product in requests.get("http://127.0.0.1:5170/api/products").json()),
-        key = lambda dp: dp.merchant_name
-    )
-
-    _SavedProductsByMerchant = {key: list(group) for key, group in groupby(_SavedProducts, key = lambda sp: sp.merchant_name)}
+    _SavedProducts = [
+        DoraProduct(**_Product)
+        for _Product
+        in requests.get("http://127.0.0.1:5170/api/products").json()
+    ]
 
     _OffersByProductID: Dict[UUID, ScrapedProductOffer] = {}
 
-    _DataProviders: List[IMerchantDataProvider] = [
-        _ServiceProvider.get_service(ColesProvider),
-        _ServiceProvider.get_service(IGAProvider),
-        _ServiceProvider.get_service(GrocerizeProvider),
-        _ServiceProvider.get_service(SaveOnGroceriesProvider),
-        _ServiceProvider.get_service(WoolworthsProvider)
-    ]
+    _MerchantMapping = {
+        SupportedMerchant.ALDI.value: SupportedMerchant.ALDI,
+        SupportedMerchant.COLES.value: SupportedMerchant.COLES,
+        SupportedMerchant.IGA.value: SupportedMerchant.IGA,
+        SupportedMerchant.WOOLWORTHS.value: SupportedMerchant.WOOLWORTHS
+    }
 
-    _HealthyDataProviders = sorted(
-        (_Provider for _Provider in _DataProviders if _Provider.is_healthy),
-        key = lambda mdp: mdp.priority
-    )
+    for _Product in _SavedProducts:
+        _Merchant = _MerchantMapping.get(_Product.merchant_name)
+        time.sleep(random.uniform(0, 0.5))
 
-    for _Merchant, _Products in _SavedProductsByMerchant.items():  # TODO: Seems redundant, but product does not have a "Merchant", only by name
-        for _Product in _Products:
+        for _MerchantDataProvider in _DataProviders:
 
-            for _MerchantDataProvider in _HealthyDataProviders:
+            if not _MerchantDataProvider.is_merchant_supported(_Merchant):
+                continue
 
-                if not _MerchantDataProvider.is_merchant_supported(_Merchant):
-                    continue
+            try:
+                if _Offer := _MerchantDataProvider.get_product(_Product):
+                    _OffersByProductID[_Product.product_id] = _Offer
+                    break
 
-                time.sleep(random.uniform(0, 0.3))
-                try:
-                    if _Offer := _MerchantDataProvider.get_product(_Product):
-                        _OffersByProductID[_Product.product_id] = _Offer
-                        break
-
-                except Exception as e:
-                    _MerchantDataProvider.is_healthy = False
-                    _Logger.exception(f"Merchant Data Provider '{_MerchantDataProvider.base_url}' encountered a problem."
-                                      f" Product: {_Product.name}. Merchant: {_Merchant}", e)
+            except Exception as e:  # TODO: Double check logger is actually logging, saw it not working
+                _MerchantDataProvider.is_healthy = False
+                _Logger.exception(f"Merchant Data Provider '{_MerchantDataProvider.base_url}' encountered a problem."
+                                  f" Product: {_Product.name}. Merchant: {_Merchant}", e)
 
     for _ProductID, _Offer in _OffersByProductID.items():
         requests.patch('http://127.0.0.1:5170/api/products', {
--- framework/merchant_api/startup.py
+++ framework/merchant_api/startup.py
@@ -3,7 +3,6 @@ import logging
 import os
 import sys
 from pathlib import Path
-from typing import List
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
@@ -11,26 +10,16 @@ from clapy import DependencyInjectorServiceProvider, IServiceProvider
 from flask import Flask
 from flask_cors import CORS
 
+from framework.merchant_api.infrastructure.merchant_data_providers import get_merchant_data_providers
+
 sys.path.append(os.getcwd())
 
 from application.infrastructure.utils import get_attributes_ending_with
-from framework.merchant_api.infrastructure.merchant_data_providers.coles_provider import \
-    ColesProvider
-from framework.merchant_api.infrastructure.merchant_data_providers.grocerize_provider import \
-    GrocerizeProvider
-from framework.merchant_api.infrastructure.merchant_data_providers.iga_provider import \
-    IGAProvider
-from framework.merchant_api.infrastructure.merchant_data_providers.save_on_groceries_provider import \
-    SaveOnGroceriesProvider
-from framework.merchant_api.infrastructure.merchant_data_providers.woolworths_provider import \
-    WoolworthsProvider
 from framework.merchant_api.infrastructure.middleware import MIDDLEWARE
 from framework.merchant_api.infrastructure.service_collection_builder import \
     ServiceCollectionBuilder
 from framework.merchant_api.services.iconfiguration_manager import \
     IConfigurationManager
-from framework.merchant_api.services.imerchant_data_provider import \
-    IMerchantDataProvider
 
 
 async def startup():
@@ -70,23 +59,12 @@ def register_api_infrastructure(app: Flask):
 
 
 def data_provider_health_check(service_provider: IServiceProvider):
-
-    # TODO: Maybe make this a route that can be hit, also just call the route from here?
-    # TODO: May need a delay here too
-
     _ConfigurationManager: IConfigurationManager = service_provider.get_service(IConfigurationManager)
     _Logger: logging.Logger = service_provider.get_service(logging.Logger)
-    _MerchantDataProviders: List[IMerchantDataProvider] = [
-        service_provider.get_service(ColesProvider),
-        service_provider.get_service(IGAProvider),
-        service_provider.get_service(GrocerizeProvider),
-        service_provider.get_service(SaveOnGroceriesProvider),
-        service_provider.get_service(WoolworthsProvider)
-    ]
-
+    _DataProviders = get_merchant_data_providers()
     _Merchants = _ConfigurationManager.get_all_merchants()
 
-    for _MerchantDataProvider in _MerchantDataProviders:
+    for _MerchantDataProvider in _DataProviders:
 
         try:
             _Offers = _MerchantDataProvider.search_by_term(

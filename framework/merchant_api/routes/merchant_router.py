9e84365855c5a6e558e7751245317e2001899fcd
Ben <ben.talese@gmail.com>
2024-07-26 21:12:33 +1000

    Add todos

--- framework/merchant_api/routes/merchant_router.py
+++ framework/merchant_api/routes/merchant_router.py
@@ -10,4 +10,12 @@ MERCHANT_ROUTER = Blueprint("MERCHANT_ROUTER", __name__, url_prefix="/api/mercha
 async def get_merchants_async():
     _ServiceProvider: IServiceProvider = current_app.service_provider
     _ConfigurationManager: IConfigurationManager = _ServiceProvider.get_service(IConfigurationManager)
+    # TODO: This should just get all merchants, and the api service can send a query to
+    # filter down the result, or filter it in memory if not bothered to implement filtering middleware here
     return jsonify([{"name": merchant.name.value} for merchant in _ConfigurationManager.get_enabled_merchants()])
+
+
+@MERCHANT_ROUTER.route("", methods = ["PATCH"])
+async def update_merchant_async():
+    # TODO: Implement enabling/disabling merchants
+    pass

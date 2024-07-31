a226abe9f428c407e5501a43b5fbf2699c0de7f3
Atif <atif.onat@studytafensw.edu.au>
2024-08-01 06:43:53 +1000

    add productStockItems to stockItemStore and misc abc

--- framework/web_app/src/stores/stockItemStore.ts
+++ framework/web_app/src/stores/stockItemStore.ts
@@ -2,15 +2,55 @@ import { defineStore } from 'pinia';
 import type { StockItem } from 'src/models/stockItem';
 import StockItemApiService, { CreateStockItemCommand, UpdateStockItemCommand } from 'src/services/api/stockItemApiService';
 import { clearRollbacks, registerRollback } from 'src/services/errorHandling/rollbackRegistry';
-import { readonly, Ref, ref } from 'vue';
+import { computed, readonly, Ref, ref } from 'vue';
+import { Product } from 'src/models/product';
+import { useProductStore } from './productStore';
+
+export type ProductStockItem = {
+    Product: Product
+    StockItem?: StockItem
+};
 
 const stockItemApiService = new StockItemApiService();
 
 export const useStockItemStore = defineStore('stockItem', () => {
 
+    //#region Composing Stores
+
+    const productStore = useProductStore();
+
+    //#endregion Composing Stores
+
+    //#region Product Stock Items
+
+    const productStockItems: Ref<ProductStockItem[]> = ref([])
+
+    const createProductStockItem = (stockItem: StockItem): void => {
+        const products = productStore.products?.filter(p =>
+            stockItem.product_ids.some(pid => pid == p.product_id));
+
+        // Possible UI binding downside. Products without stock items are not in the collection.
+        products?.forEach(p => productStockItems.value.push({
+            Product: p,
+            StockItem: stockItem
+        }))
+    };
+
+    //TODO: Compare with async performance
+    const getProductStockItems = computed((): ProductStockItem[] => {
+        stockItems.value.forEach(si => createProductStockItem(si));
+        return productStockItems.value;
+    });
+
+    //#endregion Product Stock Items
+
     //#region Stock Items
 
     const stockItems: Ref<StockItem[]> = ref([])
+
+    const createStockItemAsync = (stockItemToCreate: CreateStockItemCommand) => stockItemApiService
+        .createAsync(stockItemToCreate)
+        .then(async (resource) => stockItems.value.push((await stockItemApiService.getAsync(resource.id))[0]))
 
     const getStockItemsAsync = () => stockItemApiService
         .getAllAsync()
@@ -23,10 +63,6 @@ export const useStockItemStore = defineStore('stockItem', () => {
 
     const getStockItemAsync = (stockItemID: string) => stockItemApiService
         .getAsync(stockItemID)
-
-    const createStockItemAsync = (stockItemToCreate: CreateStockItemCommand) => stockItemApiService
-        .createAsync(stockItemToCreate)
-        .then(async (resource) => stockItems.value.push((await stockItemApiService.getAsync(resource.id))[0]))
 
     async function updateStockLevelAsync(stockItemToUpdate: UpdateStockItemCommand) {
         const stockItemIndex = stockItems.value.findIndex(si => si.stock_item_id == stockItemToUpdate.stock_item_id);
@@ -46,6 +82,9 @@ export const useStockItemStore = defineStore('stockItem', () => {
     //#endregion Stock Items
 
     return {
+        productStockItems: readonly(productStockItems),
+        getProductStockItems,
+
         stockItems: readonly(stockItems),
         getStockItemsAsync,
         createStockItemAsync,

8021aba6b819b7feffd56cb3de9054c3da441505
Ben <ben.talese@gmail.com>
2024-08-10 16:04:26 +1000

    Delete unused properties

--- framework/merchant_api/domain/entities/iga_product_offer.py
+++ framework/merchant_api/domain/entities/iga_product_offer.py
@@ -1,39 +1,23 @@
-from typing import Any, Optional
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class IGAProductOffer(BaseModel, extra='allow'):
     class Size(BaseModel, extra='allow'):
-        abbreviation: str  # "ml"
-        label: str  # "Millilitre"
-        size: float  # 500
-        type: str  # "millilitre"
+        abbreviation: str
+        size: float
 
-    # attributes: ...
     available: bool
-    barcode: str
     brand: str
-    # categories: ...
-    # defaultCategory: ...
-    description: str
-    image: dict[str, Optional[str]] = None  # TODO: How do images work here?
-    isFavorite: bool
-    isPastPurchased: bool
+    image: dict[str, Optional[str]] = None
     name: str
-    price: str  # "$3.20" n.b. omitted due to clash with class-property
-    priceLabel: str
-    priceNumeric: float  # 3.2
+    price: str
+    priceNumeric: float
     pricePerUnit: Optional[str] = None  # "$0.64/100ml"
-    priceSource: str
     productId: str
-    sellBy: str
-    sku: str
-    # tprPrice: ...
-    # unitOfMeasure: ...
-    # unitOfPrice: ...
     unitOfSize: Size
     wasPrice: Optional[str] = None
     wasPriceNumeric: Optional[float] = None
     wasWholePrice: Optional[float] = None
-    weightIncrement: Any  # TODO: ?
     wholePrice: float
--- framework/merchant_api/domain/entities/woolworths_product_offer.py
+++ framework/merchant_api/domain/entities/woolworths_product_offer.py
@@ -2,15 +2,15 @@ from pydantic import BaseModel
 
 
 class WoolworthsProductOffer(BaseModel, extra='allow'):
-    Brand: str  # 'Lindt'
+    Brand: str
     CupString: str  # '$0.80 / 100G'
     IsAvailable: bool
     InstoreCupString: str
     InstoreIsAvailable: bool
     InstorePrice: float
     InstoreWasPrice: float
-    LargeImageFile: str  # 'https://cdn0.woolworths.media/content/wowproductimages/large/114682.jpg'
-    Name: str  # 'Lindt Lindor Milk Chocolate Balls'
+    LargeImageFile: str
+    Name: str
     PackageSize: str  # '333G'
     Price: float
     Stockcode: int

ad7c6d3df6797f02b6b916091229b264bec82a0e
Ben <ben.talese@gmail.com>
2025-02-01 16:55:51 +1100

    Update main toolbar

--- framework/web_app/src/layouts/MainLayout.vue
+++ framework/web_app/src/layouts/MainLayout.vue
@@ -5,22 +5,16 @@
                 <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
                 <q-toolbar-title>
-                    <q-avatar>
-                        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+                    <q-avatar square>
+                        <img src="../../src/assets/dora-logo.png">
                     </q-avatar>
-                    Quasar App
+                    Discount Dora
                 </q-toolbar-title>
-
-                <div>Quasar v{{ $q.version }}</div>
             </q-toolbar>
         </q-header>
 
         <q-drawer v-model="leftDrawerOpen" show-if-above bordered side="left">
             <q-list>
-                <q-item-label header>
-                    Essential Links
-                </q-item-label>
-
                 <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
             </q-list>
         </q-drawer>

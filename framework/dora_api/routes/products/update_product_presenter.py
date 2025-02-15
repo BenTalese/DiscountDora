b212f0db23f655fd52385ce248259341be3fbec6
BenTalese <ben.talese@gmail.com>
2025-02-16 02:30:42 +1100

    Utilise new not found method

--- framework/dora_api/routes/products/update_product_presenter.py
+++ framework/dora_api/routes/products/update_product_presenter.py
@@ -5,6 +5,7 @@ from application.services.ipersistence_context import IPersistenceContext
 from application.use_cases.products.update_product.iupdate_product_output_port import \
     IUpdateProductOutputPort
 from domain.entities.base_entity import EntityID
+from domain.entities.product import Product
 from framework.dora_api.infrastructure.base_presenter import BasePresenter
 
 
@@ -14,7 +15,7 @@ class UpdateProductPresenter(BasePresenter, IUpdateProductOutputPort):
         self.persistence_context = persistence_context
 
     async def present_product_not_found_async(self, product_id: EntityID):
-        await self.entity_existence_failure_async(nameof(product_id), product_id.value)
+        await self.not_found_async(nameof(Product), product_id.value, 0)
 
     async def present_product_updated_async(self, product: ProductDto):
         await self.persistence_context.save_changes_async()

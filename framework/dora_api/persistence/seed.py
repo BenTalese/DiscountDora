705debb2fd11730939eca56a4b37f9fd420f04a1
BenTalese <ben.talese@gmail.com>
2025-02-16 17:08:26 +1100

    Fix seed data to make more sense

--- framework/dora_api/persistence/seed.py
+++ framework/dora_api/persistence/seed.py
@@ -82,17 +82,20 @@ async def seed_dev_data_async(persistence: IPersistenceContext):
     persistence.add(stock_level_four)
 
     stock_item_one = generate_entity(StockItem)
+    stock_item_one.name = "Kensington Pride Mangoes"
     stock_item_one.stock_location = stock_location_one
     stock_item_one.stock_level = stock_level_one
     persistence.add(stock_item_one)
 
     stock_item_two = generate_entity(StockItem)
+    stock_item_two.name = "Super Awesome Pizza"
     stock_item_two.stock_location = stock_location_one
     stock_item_two.stock_level = stock_level_two
     persistence.add(stock_item_two)
     persistence.add(stock_item_one)
 
     stock_item_three = generate_entity(StockItem)
+    stock_item_three.name = "Hot Crispy Chippies"
     stock_item_three.stock_location = stock_location_one
     stock_item_three.stock_level = stock_level_three
     persistence.add(stock_item_three)

7a859f58ff102d4389f539cce1eb34cbaa870475
BenTalese <ben.talese@gmail.com>
2025-02-18 21:52:37 +1100

    Fix not found response

--- framework/dora_api/routes/stock_locations/delete_stock_location_presenter.py
+++ framework/dora_api/routes/stock_locations/delete_stock_location_presenter.py
@@ -18,4 +18,4 @@ class DeleteStockLocationPresenter(BasePresenter, IDeleteStockLocationOutputPort
         await self.no_content_async()
 
     async def present_stock_location_not_found_async(self, stock_location_id: EntityID):
-        await self.entity_existence_failure_async(nameof(StockLocation), nameof(stock_location_id), stock_location_id.value)
+        await self.not_found_async(nameof(StockLocation), stock_location_id.value, 0)

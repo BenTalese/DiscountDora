e13abdbc5c5408fcc7f60330d1ae300ea4bacf79
BenTalese <ben.talese@gmail.com>
2025-03-08 15:10:22 +1100

    Add required index page

---
+++ framework/web_app/src/pages/IndexPage.vue
@@ -0,0 +1,6 @@
+<!-- This page doesn't seem to do anything except keep tsconfig.json happy? -->
+<template>
+    <q-page class="row items-center justify-evenly"></q-page>
+</template>
+
+<script setup lang="ts"></script>

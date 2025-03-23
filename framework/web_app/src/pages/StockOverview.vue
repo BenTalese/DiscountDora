22f4baf0eadbd9c7c203522130a12fa17e2de7c0
BenTalese <ben.talese@gmail.com>
2025-03-23 18:35:04 +1100

    Use components and restructure

--- framework/web_app/src/pages/StockOverview.vue
+++ framework/web_app/src/pages/StockOverview.vue
@@ -1,26 +1,7 @@
 <template>
-    <q-btn
-        class="q-ma-sm"
-        @click="onCreateStockItemButtonClick"
-        color="positive"
-    >
-        <q-icon
-            name="add"
-            color="blue-grey-10"
-            size="30px"
-        />
-    </q-btn>
-    <q-btn
-        class="q-ma-sm"
-        @click="onCreateStockItemButtonClick"
-        color="secondary"
-    >
-        <q-icon
-            name="add"
-            color="green"
-            size="30px"
-        />
-    </q-btn>
+    <!-- #region Toolbar -->
+    <AddToolbarButton @click="onAddButtonClick" />
+    <!-- #endregion Toolbar -->
 
     <q-card
         class="no-shadow q-ma-sm"
@@ -98,186 +79,39 @@
         </q-card-section>
     </q-card>
 
-    <q-dialog
-        @hide="clearCreateStockItemForm"
-        v-model="shouldDisplayCreateStockItemModal"
-    >
-        <q-card
-            class="q-pa-md"
-            style="width: 700px; max-width: 80vw"
-        >
-            <p class="text-h4">Add a new stock item</p>
-            <q-form
-                class="q-gutter-md"
-                @submit="createStockItemAsync(createStockItemForm)"
-            >
-                <q-input
-                    :rules="nameInputRules"
-                    autofocus
-                    filled
-                    label="Name"
-                    v-model="createStockItemForm.name"
-                />
-
-                <q-input
-                    type="number"
-                    filled
-                    hint="Alerts disabled if set to zero"
-                    label="Days Until Stocktake Alert"
-                    v-model="createStockItemForm.days_until_stocktake_alert"
-                />
-
-                <q-select
-                    :option-label="getStockLevelName"
-                    :option-value="getStockLevelID"
-                    :options="stockLevelStore.stockLevels"
-                    :rules="stockLevelSelectRules"
-                    emit-value
-                    label="Stock Level"
-                    map-options
-                    v-model="createStockItemForm.stock_level_id"
-                >
-                    <template v-slot:option="scope">
-                        <q-item v-bind="scope.itemProps">
-                            <q-item-section avatar>
-                                <q-avatar
-                                    :color="getStockLevelColour(scope.opt.stock_level_id)"
-                                    size="25px"
-                                />
-                            </q-item-section>
-                            <q-item-section>
-                                <q-item-label>
-                                    {{ scope.opt.name }}
-                                </q-item-label>
-                            </q-item-section>
-                        </q-item>
-                    </template>
-                </q-select>
-
-                <!-- TODO: IMPLEMENT -->
-                <q-select
-                    label="Stock Group"
-                    v-model="createStockItemForm.stock_group_id"
-                ></q-select>
-
-                <!-- TODO: IMPLEMENT -->
-                <q-select
-                    label="Stock Location"
-                    v-model="createStockItemForm.stock_location_id"
-                ></q-select>
-
-                <q-card-actions align="right">
-                    <q-btn
-                        type="submit"
-                        align="right"
-                        color="info"
-                        label="Save & Continue"
-                        size="lg"
-                    />
-                    <q-btn
-                        type="submit"
-                        align="right"
-                        color="positive"
-                        label="Save & Close"
-                        size="lg"
-                        v-close-popup
-                    />
-                </q-card-actions>
-            </q-form>
-        </q-card>
-    </q-dialog>
-
-    <q-dialog v-model="shouldDisplayAddToShoppingCartModal">
-        <q-card style="width: 700px; max-width: 80vw">
-            <q-card-section class="row items-center">
-                <span class="q-ml-sm">Add {{ 'stock item' }} to a shopping list.</span>
-            </q-card-section>
-
-            <q-card-actions align="right">
-                <q-btn
-                    color="primary"
-                    flat
-                    label="Cancel"
-                    v-close-popup
-                />
-                <q-btn
-                    color="primary"
-                    flat
-                    label="Add to list"
-                    v-close-popup
-                />
-            </q-card-actions>
-        </q-card>
-    </q-dialog>
+    <!-- #region Modals -->
+    <CreateStockItemModal v-model="shouldDisplayCreateStockItemModal" />
+    <AddToShoppingCartModal v-model="shouldDisplayAddToShoppingCartModal" />
+    <!-- #endregion Modals -->
 </template>
 
 <script lang="ts" setup>
+    //#region Imports
     import { storeToRefs } from 'pinia';
-    import type { ValidationRule } from 'quasar';
+    import AddToShoppingCartModal from 'src/components/modal/AddToShoppingCartModal.vue';
+    import CreateStockItemModal from 'src/components/modal/CreateStockItemModal.vue';
+    import AddToolbarButton from 'src/components/toolbar/AddToolbarButton.vue';
     import { getStockLevelColour } from 'src/helpers/stockLevelLogic';
-    import type { StockLevel } from 'src/models/stockLevel';
-    import type { CreateStockItemCommand } from 'src/services/api/stockItemApiService';
     import { useStockItemStore } from 'src/stores/stockItemStore';
     import { useStockLevelStore } from 'src/stores/stockLevelStore';
-    import { reactive, ref } from 'vue';
+    import { ref } from 'vue';
+    //#endregion Imports
 
-    //#region Common
-
+    //#region Store Initialisation
     const stockItemStore = useStockItemStore();
     const stockLevelStore = useStockLevelStore();
-
     const { stockItems } = storeToRefs(stockItemStore);
-    const { createStockItemAsync, updateStockLevelAsync } = stockItemStore;
-
     const { stockLevels } = storeToRefs(stockLevelStore);
+    const { updateStockLevelAsync } = stockItemStore;
+    //#endregion Store Initialisation
 
-    //#endregion Common
-
-    //#region Create Stock Item
-
+    //#region State
     const shouldDisplayCreateStockItemModal = ref(false);
-
-    const defaultCreateStockItemForm = {
-        days_until_stocktake_alert: 0,
-        name: '',
-        stock_group_id: null,
-        stock_level_id: stockLevels.value[0]!.stock_level_id,
-        stock_location_id: null
-    };
-
-    const createStockItemForm: CreateStockItemCommand = reactive({
-        ...defaultCreateStockItemForm
-    });
-
-    function clearCreateStockItemForm() {
-        Object.assign(createStockItemForm, defaultCreateStockItemForm);
-    }
-
-    const onCreateStockItemButtonClick = () => (shouldDisplayCreateStockItemModal.value = true);
-
-    //#endregion Create Stock Item
-
-    //#region Create Stock Item Rules
-
-    const nameInputRules: ValidationRule[] = [(val: string) => (val && val.length > 0) || 'Please type something'];
-
-    const stockLevelSelectRules: ValidationRule[] = [(val: string) => !!val || 'Please select a stock level'];
-
-    //#endregion Create Stock Item Rules
-
-    //#region Shopping List
-
     const shouldDisplayAddToShoppingCartModal = ref(false);
+    //#endregion State
 
+    //#region Methods
+    const onAddButtonClick = () => (shouldDisplayCreateStockItemModal.value = true);
     const onShoppingListButtonClick = () => (shouldDisplayAddToShoppingCartModal.value = true);
-
-    //#endregion Shopping List
-
-    //#region Stock Level
-
-    const getStockLevelID = (stockLevel: StockLevel) => stockLevel.stock_level_id;
-
-    const getStockLevelName = (stockLevel: StockLevel) => stockLevel.name;
-
-    //#endregion Stock Level
+    //#endregion Methods
 </script>

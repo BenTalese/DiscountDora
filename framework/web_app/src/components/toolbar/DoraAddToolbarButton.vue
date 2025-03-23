0c145b8fc785ca0f8756f78182d5703922b82ad6
BenTalese <ben.talese@gmail.com>
2025-03-23 17:50:43 +1100

    Create add toolbar button

---
+++ framework/web_app/src/components/toolbar/DoraAddToolbarButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <DoraToobarButton
+        back-colour="positive"
+        icon="add"
+        icon-colour="blue-grey-10"
+        @click="handleClick"
+    />
+
+    <!-- TODO: Below is alternate styling idea -->
+    <!-- <DoraToobarButton
+        back-colour="secondary"
+        icon="add"
+        icon-colour="green"
+        @click="handleClick"
+    /> -->
+</template>
+
+<script lang="ts" setup>
+    //#region Imports
+    import DoraToobarButton from './DoraToolbarButton.vue';
+    //#endregion Imports
+
+    //#region Props and Events
+    const props = defineProps({
+        onClick: { type: Function, required: true }
+    });
+    //#endregion Props and Events
+
+    //#region Methods
+    const handleClick = () => props.onClick();
+    //#endregion Methods
+</script>
---
+++ framework/web_app/src/components/toolbar/DoraToolbarButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <q-btn
+        class="q-ma-sm"
+        @click="handleClick"
+        :color="props.backColour"
+        :label="props.text"
+    >
+        <q-icon
+            v-if="props.icon"
+            :name="props.icon"
+            :color="props.iconColour"
+            size="30px"
+        />
+        <q-tooltip v-if="props.toolTip">
+            {{ props.toolTip }}
+        </q-tooltip>
+    </q-btn>
+</template>
+
+<script lang="ts" setup>
+    //#region Props and Events
+    const props = defineProps({
+        backColour: { type: String, required: true },
+        icon: {
+            type: String,
+            required: false,
+            validator(value: string, props: { iconColour: string }) {
+                return !!value === !!props.iconColour;
+            }
+        },
+        iconColour: {
+            type: String,
+            required: false,
+            validator(value: string, props: { icon: string }) {
+                return !!value === !!props.icon;
+            }
+        },
+        onClick: { type: Function, required: true },
+        text: { type: String, required: false },
+        toolTip: { type: String, required: false }
+    });
+    //#endregion Props and Events
+
+    //#region Methods
+    const handleClick = () => props.onClick();
+    //#endregion Methods
+</script>

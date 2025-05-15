3fc7efa5d5326924d9ddd8917c9ea9deac730f48
BenTalese <ben.talese@gmail.com>
2025-05-15 20:04:06 +1000

    Include all icons

--- framework/web_app/quasar.config.ts
+++ framework/web_app/quasar.config.ts
@@ -30,17 +30,19 @@ export default defineConfig((ctx) => {
         ],
 
         // https://github.com/quasarframework/quasar/tree/dev/extras
+        // USAGE: https://quasar.dev/vue-components/icon#webfont-usage
         extras: [
-            // 'ionicons-v4',
-            // 'mdi-v7',
-            // 'fontawesome-v6',
-            // 'eva-icons',
-            // 'themify',
-            // 'line-awesome',
-            // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
+            'bootstrap-icons',
+            'eva-icons',
+            'fontawesome-v6',
+            'ionicons-v4',
+            'line-awesome',
+            'material-icons',
+            'mdi-v7',
+            'themify'
 
-            'roboto-font', // optional, you are not bound to it
-            'material-icons' // optional, you are not bound to it
+            // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
+            // 'roboto-font', // optional, you are not bound to it
         ],
 
         // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build

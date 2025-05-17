8ad4c0e8f6adfc55a207578cc8096703a5bd4af4
BenTalese <ben.talese@gmail.com>
2025-05-17 11:21:25 +1000

    Implement new layout with components

--- framework/web_app/src/layouts/MainLayout.vue
+++ framework/web_app/src/layouts/MainLayout.vue
@@ -1,54 +1,68 @@
 <template>
     <q-layout
         view="hHh LpR lFf"
-        class="scrollable"
+        class="dora-scrollbarOverride"
     >
         <q-header
             bordered
             class="bg-primary text-white"
         >
-            <q-toolbar style="height: 60px">
-                <q-btn
-                    @click="toggleLeftDrawer"
-                    aria-label="Menu"
-                    dense
-                    flat
-                    icon="menu"
-                    round
-                    v-if="$q.screen.lt.md"
+            <q-toolbar class="dora-titlebar">
+                <HamburgerButton
+                    :is-visible="$q.screen.lt.md"
+                    :onClick="toggleLeftDrawer"
                 />
 
-                <q-toolbar-title class="dora-fontFamily-cuteDino dora-fontSize-30">
-                    <q-avatar
-                        square
-                        size="50px"
-                        class="q-ma-xs"
-                    >
-                        <img src="../../src/assets/logo-mascot.png" />
-                    </q-avatar>
-                    <p
-                        class="dora-marginTop-5 dora-marginBottom-0 dora-marginLeft-15 dora-display-inlineBlock text-accent"
-                    >
-                        {{ route.meta.title }}
-                    </p>
-                </q-toolbar-title>
+                <ApplicationLogo />
+
+                <ApplicationTitle
+                    :label="route.meta.title"
+                    shouldShrink
+                />
+
+                <MainMenuButtonStrip
+                    :is-visible="$q.screen.gt.sm"
+                    :menu-links="linksList"
+                />
             </q-toolbar>
         </q-header>
 
         <q-drawer
+            v-if="$q.screen.lt.md"
             v-model="leftDrawerOpen"
             show-if-above
             bordered
             side="left"
         >
             <q-list>
-                <EssentialLink
+                <SideMenuButton
                     v-for="link in linksList"
-                    :key="link.title"
+                    :key="link.label"
                     v-bind="link"
                 />
             </q-list>
         </q-drawer>
+
+        <!-- TODO: User option: Show menu only on side (no big buttons) -->
+        <!-- <q-drawer
+            v-if="$q.screen.lt.md"
+            v-model="leftDrawerOpen"
+            show-if-above
+            :mini="miniState"
+            @mouseenter="miniState = false"
+            @mouseleave="miniState = true"
+            mini-to-overlay
+            bordered
+            side="left"
+        >
+            <q-list>
+                <SideMenuButton
+                    v-for="link in linksList"
+                    :key="link.label"
+                    v-bind="link"
+                />
+            </q-list>
+        </q-drawer> -->
 
         <q-page-container>
             <router-view />
@@ -57,27 +71,78 @@
 </template>
 
 <script setup lang="ts">
-    import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
+    //#region Imports
+    import ApplicationLogo from 'src/components/menu/ApplicationLogo.vue';
+    import ApplicationTitle from 'src/components/menu/ApplicationTitle.vue';
+    import HamburgerButton from 'src/components/menu/HamburgerButton.vue';
+    import MainMenuButtonStrip from 'src/components/menu/MainMenuButtonStrip.vue';
+    import type { MenuButtonProps } from 'src/components/menu/menuButtonProps';
+    import SideMenuButton from 'src/components/menu/SideMenuButton.vue';
     import { ref } from 'vue';
+    import { useRoute } from 'vue-router';
+    const route = useRoute();
+    //#endregion Imports
 
-    const linksList: EssentialLinkProps[] = [
+    //#region State
+    // TODO: See above 'TODO'
+    // const miniState = ref(true);
+
+    const linksList: MenuButtonProps[] = [
         {
-            title: 'Stock',
-            icon: 'stock',
-            link: 'http://localhost:5174/#'
+            icon: 'mdi-view-dashboard',
+            label: 'Dashboard',
+            link: '/dashboard'
         },
         {
-            title: 'Product Search',
+            // icon: 'stock',
+            // icon: 'inventory',
+            // icon: 'kitchen',
+            // icon: 'storefront',
+            // icon: 'fa-solid fa-cubes-stacked',
+            icon: 'mdi-chart-box',
+            label: 'Stock',
+            link: '/stock'
+        },
+        {
             icon: 'search',
-            link: 'http://localhost:5174/#/product-search'
+            label: 'Product Search',
+            link: '/product-search'
+        },
+        {
+            icon: 'shopping_cart',
+            label: 'My Shopping',
+            link: '/shopping'
+        },
+        {
+            icon: 'circle',
+            label: 'My Products',
+            link: '/products'
+        },
+        {
+            icon: 'water',
+            label: 'Meal Planner',
+            link: '/meals'
+        },
+        {
+            icon: 'book',
+            label: 'Recipe Book',
+            link: '/recipes'
+        },
+        {
+            icon: 'settings',
+            label: 'Settings',
+            link: '/settings'
         }
     ];
 
     const leftDrawerOpen = ref(false);
+    //#endregion State
 
+    //#region Methods
     function toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value;
     }
+    //#endregion Methods
 </script>
 
 <style scoped lang="scss">

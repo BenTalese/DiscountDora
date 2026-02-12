fe66d2e812b0984cf90146c27859cf8631104138
BenTalese <ben.talese@gmail.com>
2026-02-13 08:30:09 +1100

    Migrate exceptions to one file

--- dora_api/domain/exceptions.py
+++ dora_api/domain/exceptions.py
@@ -1,3 +1,8 @@
+class MappingError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
 class PersistenceError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
--- dora_api/domain/exceptions/mapping_error.py
+++
@@ -1,3 +0,0 @@
-class MappingError(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)

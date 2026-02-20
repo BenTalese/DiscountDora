9c22b1fe4574ac0a1d0523b134878f427f3ae76f
BenTalese <ben.talese@gmail.com>
2026-02-20 12:25:43 +1100

    Move exception classes

--- dora_api/domain/exceptions.py
+++ dora_api/domain/exceptions.py
@@ -6,3 +6,13 @@ class MappingError(Exception):
 class PersistenceError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
+
+
+class DependencyConstructionError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class DuplicateServiceError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)

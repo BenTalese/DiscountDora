b8f5f748504e09008a5b634a08049965f6345617
BenTalese <ben.talese@gmail.com>
2026-02-21 22:35:49 +1100

    Rewrite get users

--- dora_api/features/routers.py
+++ dora_api/features/routers.py
@@ -3,3 +3,4 @@ from flask import Blueprint
 HEALTH_ROUTER = Blueprint("HEALTH_ROUTER", __name__, url_prefix="/api/health")
 MERCHANT_ROUTER = Blueprint("MERCHANT_ROUTER", __name__, url_prefix="/api/merchants")
 STOCK_ITEM_ROUTER = Blueprint("STOCK_ITEM_ROUTER", __name__, url_prefix="/api/stock-items")
+USER_ROUTER = Blueprint("USER_ROUTER", __name__, url_prefix="/api/users")
---
+++ dora_api/features/users/get_users.py
@@ -0,0 +1,49 @@
+import logging
+from dataclasses import dataclass
+from typing import List
+from uuid import UUID
+
+from domain.entities.user import User
+
+from dora_api.features.routers import USER_ROUTER
+from dora_api.infrastructure.api_response import ok
+from dora_api.infrastructure.decorators import has_response
+from dora_api.infrastructure.utils import get_container
+from dora_api.services.irepository import IRepository
+
+
+@dataclass(frozen=True, slots=True)
+class UserDto:
+    email: str | None
+    send_deals_on_day: int
+    username: str
+    user_id: UUID
+
+    @classmethod
+    def from_entity(cls, user: User) -> 'UserDto':
+        return UserDto(
+            email = user.email,
+            send_deals_on_day = user.send_deals_on_day,
+            username = user.username,
+            user_id = user.id.value
+        )
+
+
+class GetUsersHandler:
+    def __init__(self, repository: IRepository[User]):
+        self.repository = repository
+
+    def handle(self) -> List[UserDto]:
+        return self.repository.get().project(UserDto.from_entity)
+
+
+@USER_ROUTER.route("")
+@USER_ROUTER.route("<query>")
+@has_response(UserDto)
+def get_users(query: str | None = None):
+    _Logger = logging.getLogger(__name__)
+    _Logger.info("Received request to get users.")
+    _Handler = get_container().inject(GetUsersHandler)
+    _Result = _Handler.handle()
+    _Logger.info(f"Successfully retrieved {len(_Result)} users.")
+    return ok(_Result)

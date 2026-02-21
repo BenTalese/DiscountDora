31c3ced18d4fac0b562cb9b1ede10a94002fff58
BenTalese <ben.talese@gmail.com>
2026-02-22 10:09:53 +1100

    Rewrite get stock levels

--- dora_api/features/routers.py
+++ dora_api/features/routers.py
@@ -3,4 +3,5 @@ from flask import Blueprint
 HEALTH_ROUTER = Blueprint("HEALTH_ROUTER", __name__, url_prefix="/api/health")
 MERCHANT_ROUTER = Blueprint("MERCHANT_ROUTER", __name__, url_prefix="/api/merchants")
 STOCK_ITEM_ROUTER = Blueprint("STOCK_ITEM_ROUTER", __name__, url_prefix="/api/stock-items")
+STOCK_LEVEL_ROUTER = Blueprint("STOCK_LEVEL_ROUTER", __name__, url_prefix="/api/stock-levels")
 USER_ROUTER = Blueprint("USER_ROUTER", __name__, url_prefix="/api/users")
---
+++ dora_api/features/stock_levels/get_stock_levels.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+import logging
+from typing import List
+from uuid import UUID
+
+from domain.entities.stock_level import StockLevel
+
+from dora_api.features.routers import STOCK_LEVEL_ROUTER
+from dora_api.infrastructure.api_response import ok
+from dora_api.infrastructure.decorators import has_response
+from dora_api.infrastructure.utils import get_container
+from dora_api.services.irepository import IRepository
+
+
+@dataclass(frozen=True, slots=True)
+class StockLevelDto:
+    name: str
+    sequence: int
+    stock_level_id: UUID
+
+    @classmethod
+    def from_entity(cls, stock_level: StockLevel) -> 'StockLevelDto':
+        return StockLevelDto(
+            name = stock_level.name,
+            sequence = stock_level.sequence,
+            stock_level_id = stock_level.id.value
+        )
+
+
+class GetStockLevelsHandler:
+    def __init__(self, repository: IRepository[StockLevel]):
+        self.repository = repository
+
+    def handle(self) -> List[StockLevelDto]:
+        return self.repository.get().project(StockLevelDto.from_entity)
+
+
+@STOCK_LEVEL_ROUTER.route("")
+@STOCK_LEVEL_ROUTER.route("<query>")
+@has_response(StockLevelDto)
+def get_stock_levels(query: str | None = None):
+    _Logger = logging.getLogger(__name__)
+    _Logger.info("Received request to get stock levels.")
+    _Handler = get_container().inject(GetStockLevelsHandler)
+    _Result = _Handler.handle()
+    _Logger.info(f"Successfully retrieved {len(_Result)} stock levels.")
+    return ok(_Result)

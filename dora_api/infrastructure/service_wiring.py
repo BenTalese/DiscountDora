4303080cb43e99a42f4f83a15f6c44f357e9bcd9
BenTalese <ben.talese@gmail.com>
2026-02-25 19:48:34 +1100

    Fix boo-boo with wiring

--- dora_api/infrastructure/service_wiring.py
+++ dora_api/infrastructure/service_wiring.py
@@ -13,6 +13,14 @@ from dora_api.domain.entities.user import User
 from dora_api.infrastructure.configuration_manager import ConfigurationManager
 from dora_api.infrastructure.dependency_container import DependencyContainer
 from dora_api.infrastructure.utils import get_classes_ending_with
+from dora_api.persistence.models.merchant_model import MerchantModel
+from dora_api.persistence.models.product_model import ProductModel
+from dora_api.persistence.models.product_offer_model import ProductOfferModel
+from dora_api.persistence.models.shopping_list_model import ShoppingListModel
+from dora_api.persistence.models.stock_item_model import StockItemModel
+from dora_api.persistence.models.stock_level_model import StockLevelModel
+from dora_api.persistence.models.stock_location_model import StockLocationModel
+from dora_api.persistence.models.user_model import UserModel
 from dora_api.persistence.sqlalchemy_repository import SqlAlchemyRepository
 from dora_api.services.iconfiguration_manager import IConfigurationManager
 from dora_api.services.irepository import IRepository
@@ -21,14 +29,14 @@ from dora_api.services.irepository import IRepository
 def build_dependency_container() -> DependencyContainer:
     container = DependencyContainer()
 
-    container.register_service(providers.Factory, SqlAlchemyRepository[Merchant], IRepository[Merchant], model_class=Merchant)
-    container.register_service(providers.Factory, SqlAlchemyRepository[Product], IRepository[Product], model_class=Product)
-    container.register_service(providers.Factory, SqlAlchemyRepository[ProductOffer], IRepository[ProductOffer], model_class=ProductOffer)
-    container.register_service(providers.Factory, SqlAlchemyRepository[ShoppingList], IRepository[ShoppingList], model_class=ShoppingList)
-    container.register_service(providers.Factory, SqlAlchemyRepository[StockItem], IRepository[StockItem], model_class=StockItem)
-    container.register_service(providers.Factory, SqlAlchemyRepository[StockLevel], IRepository[StockLevel], model_class=StockLevel)
-    container.register_service(providers.Factory, SqlAlchemyRepository[StockLocation], IRepository[StockLocation], model_class=StockLocation)
-    container.register_service(providers.Factory, SqlAlchemyRepository[User], IRepository[User], model_class=User)
+    container.register_service(providers.Factory, SqlAlchemyRepository[Merchant], IRepository[Merchant], model_class=MerchantModel)
+    container.register_service(providers.Factory, SqlAlchemyRepository[Product], IRepository[Product], model_class=ProductModel)
+    container.register_service(providers.Factory, SqlAlchemyRepository[ProductOffer], IRepository[ProductOffer], model_class=ProductOfferModel)
+    container.register_service(providers.Factory, SqlAlchemyRepository[ShoppingList], IRepository[ShoppingList], model_class=ShoppingListModel)
+    container.register_service(providers.Factory, SqlAlchemyRepository[StockItem], IRepository[StockItem], model_class=StockItemModel)
+    container.register_service(providers.Factory, SqlAlchemyRepository[StockLevel], IRepository[StockLevel], model_class=StockLevelModel)
+    container.register_service(providers.Factory, SqlAlchemyRepository[StockLocation], IRepository[StockLocation], model_class=StockLocationModel)
+    container.register_service(providers.Factory, SqlAlchemyRepository[User], IRepository[User], model_class=UserModel)
 
     container.register_service(providers.Singleton, ConfigurationManager, IConfigurationManager)
 

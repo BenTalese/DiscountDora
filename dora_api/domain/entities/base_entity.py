decfdb0fa6c889275fc4e53ff4441eae551ff53f
BenTalese <ben.talese@gmail.com>
2026-03-21 23:33:08 +1100

    Simplify base entity and remove EntityID

--- dora_api/domain/entities/base_entity.py
+++ dora_api/domain/entities/base_entity.py
@@ -1,31 +1,18 @@
 from abc import ABC
 from dataclasses import dataclass, field
-from uuid import UUID, uuid4
-
-
-@dataclass(frozen=True)
-class EntityID:
-    value: UUID = field(default_factory=uuid4)
-
-    def __composite_values__(self):
-        return (self.value,)
-
-    def __new__(cls, value: UUID | None = None):
-        obj = object.__new__(cls)
-        object.__setattr__(obj, "value", value if value is not None else uuid4())
-        return obj
+from uuid import UUID
 
 
 @dataclass(eq=False, kw_only=True)
 class BaseEntity(ABC):
-    id: EntityID = field(default_factory=EntityID)
+    id: UUID = field(default=UUID(int=0))
 
-    # def __eq__(self, other):
-    #     if self is other:
-    #         return True
-    #     if not isinstance(other, type(self)):
-    #         return False
-    #     return self.id == other.id
+    def __eq__(self, other):
+        if self is other:
+            return True
+        if not isinstance(other, type(self)):
+            return False
+        return self.id == other.id
 
-    # def __hash__(self):
-    #     return hash((type(self), self.id))
+    def __hash__(self):
+        return hash((type(self), self.id))
